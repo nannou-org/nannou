ebb62bf8285f294acaeb9b2c4d79424425614c41
mitchmindtree <mitchell.nordine@gmail.com>
2017-10-15 19:18:47 +1100

    Simplify loop_mode example a bit

--- examples/loop_mode.rs
+++ examples/loop_mode.rs
@@ -1,9 +1,9 @@
 extern crate nannou;
 
-use nannou::{App, Event, Frame, LoopMode, WindowEvent, WindowId, VirtualKeyCode, ElementState};
+use nannou::{App, Event, Frame, LoopMode, WindowEvent, WindowId, VirtualKeyCode as Key, ElementState};
 
 fn main() {
-    nannou::run::<Model, Event>(model, update, draw);
+    nannou::run(model, update, draw);
 }
 
 struct Model {
@@ -11,6 +11,7 @@ struct Model {
 }
 
 fn model(app: &App) -> Model {
+    app.set_loop_mode(LoopMode::wait(3));
     let window = app.new_window().build().unwrap();
     Model { window }
 }
@@ -19,30 +20,20 @@ fn update(app: &App, model: Model, event: Event) -> Model {
     match event {
         Event::WindowEvent(id, event) => {
             println!("{:?}", event);
-            window_event(app, model, id, event)
+            match event {
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if let (ElementState::Pressed, Some(_)) = (input.state, input.virtual_keycode) {
+                        match app.loop_mode() {
+                            LoopMode::Rate { .. } => app.set_loop_mode(LoopMode::wait(3)),
+                            LoopMode::Wait { .. } => app.set_loop_mode(LoopMode::rate_fps(60.0)),
+                        }
+                    }
+                },
+                _ => (),
+            }
         },
         Event::Update(update) => {
             println!("{:?}", &update);
-            model
-        },
-        _ => model,
-    }
-}
-
-fn window_event(app: &App, model: Model, _id: WindowId, event: WindowEvent) -> Model {
-    match event {
-        WindowEvent::KeyboardInput { input, .. } => match input.state {
-            ElementState::Pressed => match input.virtual_keycode {
-                Some(VirtualKeyCode::Space) => {
-                    match app.loop_mode() {
-                        LoopMode::Rate { .. } => app.set_loop_mode(LoopMode::wait(3)),
-                        LoopMode::Wait { .. } => app.set_loop_mode(LoopMode::rate_fps(60.0)),
-                    }
-                    println!("Switched LoopMode to: {:?}", app.loop_mode());
-                },
-                _ => (),
-            },
-            ElementState::Released => {},
         },
         _ => (),
     }
@@ -51,12 +42,6 @@ fn window_event(app: &App, model: Model, _id: WindowId, event: WindowEvent) -> M
 
 // Draw the state of your `Model` into the given `Frame` here.
 fn draw(_app: &App, model: &Model, frame: Frame) -> Frame {
-    {
-        // Our app only has one window, so retrieve this part of the `Frame`.
-        let mut window_frame = frame.window(model.window).unwrap();
-        // Paint it! (red, green, blue, alpha).
-        window_frame.clear_color(1.0, 0.0, 0.0, 1.0);
-    }
-    // Return the drawn frame.
+    frame.window(model.window).unwrap().clear_color(0.1, 0.11, 0.12, 1.0);
     frame
 }
