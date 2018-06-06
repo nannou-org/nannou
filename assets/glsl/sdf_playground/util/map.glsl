94c1faf775491954e93957ec82b22b00acc3a5f9
JoshuaBatty <joshpbatty@gmail.com>
2018-06-06 22:58:17 +0200

    removing nonsense

--- assets/glsl/sdf_playground/util/map.glsl
+++
@@ -1,4 +0,0 @@
-float map( float value, float inMin, float inMax, float outMin, float outMax ) 
-{
-    return ( (value - inMin) / ( inMax - inMin ) * ( outMax - outMin ) ) + outMin; 
-}
--- assets/glsl/sdf_playground/util/pi.glsl
+++
@@ -1,6 +0,0 @@
-#define TWO_PI 6.2831853072
-#define PI 3.141592653589793
-#define HALF_PI 1.5707963267948966
-
-#define TAU (2*PI)
-#define PHI (sqrt(5)*0.5 + 0.5)
