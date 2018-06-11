0fcc909b48ed29b08a566030e13504dd5983f0f8
mitchmindtree <mitchell.nordine@gmail.com>
2018-06-11 23:48:30 +0200

    [WIP] Begin `draw.polyline()` implementation
    Still needs an implementation of the Vertices and Indices types.

--- src/draw/properties/primitive/mod.rs
+++ src/draw/properties/primitive/mod.rs
@@ -4,6 +4,7 @@ pub mod ellipse;
 pub mod line;
 pub mod mesh;
 pub mod polygon;
+pub mod polyline;
 pub mod quad;
 pub mod rect;
 pub mod tri;
@@ -12,6 +13,7 @@ pub use self::ellipse::Ellipse;
 pub use self::line::Line;
 pub use self::mesh::Mesh;
 pub use self::polygon::Polygon;
+pub use self::polyline::Polyline;
 pub use self::quad::Quad;
 pub use self::rect::Rect;
 pub use self::tri::Tri;
@@ -30,6 +32,8 @@ pub enum Primitive<S = geom::DefaultScalar> {
     PolygonPointless(polygon::Pointless),
     PolygonFill(Polygon<polygon::Fill, S>),
     PolygonColorPerVertex(Polygon<polygon::PerVertex, S>),
+    PolylineVertexless(polyline::Vertexless),
+    Polyline(Polyline<S>),
     Quad(Quad<S>),
     Rect(Rect<S>),
     Tri(Tri<S>),
