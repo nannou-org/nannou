315dc7d3826de9fba362307e364b6c9550ce427f
mitchmindtree <mitchell.nordine@gmail.com>
2018-06-29 00:29:38 +0200

    Add `miter::Vertices` iterator. Add `next_pair` fn. ExactSizeIterators.
    - Adds a `Vertices` iterator for flattening the `VertexPairs` iterator.
    - Adds a `next_pair` function for sharing iteration logic between
    `VertexPairs` iterator and `draw.polygon().vertices` method.
    - Add ExactSizeIterator implementations for miter iterators.

--- src/geom/line/join/miter.rs
+++ src/geom/line/join/miter.rs
@@ -61,13 +61,21 @@ use math::{BaseFloat, Point2};
 /// }
 /// ```
 #[derive(Clone, Debug)]
-pub struct Vertices<I, S> {
+pub struct VertexPairs<I, S> {
     half_thickness: S,
     points: I,
     // The point yielded prior to `point_b`.
     point_a: Option<Point2<S>>,
     // The point for which each iteration yields normal vertices.
     point_b: Option<Point2<S>>,
+}
+
+/// The same as the **VertexPairs** iterator, but flattened in order to produce a single vertex at
+/// a time.
+#[derive(Clone, Debug)]
+pub struct Vertices<I, S> {
+    pairs: VertexPairs<I, S>,
+    next: Option<Point2<S>>,
 }
 
 /// An iterator yielding the indices necessary to triangulate a miter polyline from a sequence of
@@ -80,7 +88,7 @@ pub struct TriangleIndices {
     second: bool,
 }
 
-impl<I, S> Vertices<I, S> {
+impl<I, S> VertexPairs<I, S> {
     /// Produces an iterator yeilding the miter normal vertices for each point in the given
     /// iterator.
     pub fn new<P>(points: P, half_thickness: S) -> Self
@@ -92,11 +100,20 @@ impl<I, S> Vertices<I, S> {
         let mut points = points.into_iter();
         let point_a = None;
         let point_b = points.next();
-        Vertices {
+        VertexPairs {
             half_thickness,
             points,
             point_a,
             point_b,
+        }
+    }
+
+    /// Flatten this iterator yielding vertex pairs into an iterator that yields a single vertex at
+    /// a time.
+    pub fn flatten(self) -> Vertices<I, S> {
+        Vertices {
+            pairs: self,
+            next: None,
         }
     }
 }
@@ -111,78 +128,147 @@ impl TriangleIndices {
     }
 }
 
-impl<I, S> Iterator for Vertices<I, S>
+impl<I, S> Iterator for VertexPairs<I, S>
 where
     I: Iterator<Item = Point2<S>>,
     S: BaseFloat,
 {
     type Item = [Point2<S>; 2];
     fn next(&mut self) -> Option<Self::Item> {
-        let Vertices {
+        let VertexPairs {
             half_thickness,
             ref mut points,
             ref mut point_a,
             ref mut point_b,
         } = *self;
+        next_pair(half_thickness, point_a, point_b, points.next())
+    }
+}
 
-        let a = point_a.take();
-        let b = point_b.take();
+/// Determine the next vertex pair in the miter sequence given the two previous points and the next
+/// if one exists.
+///
+/// - `half_thickness`: half the thickness of the line.
+/// - `point_a`: the point in the sequence prior to the last point.
+/// - `point_b`: the last point in the sequence.
+/// - `next_point`: the next point in the sequence.
+///
+/// This function expects that `point_a` is `None` and `point_b` is `Some` (containing the first
+/// point in the sequence) when beginning the sequence.
+///
+/// This function is used within both the `VertexPairs` iterator and the `polyline().vertices()`
+/// draw builder method.
+pub fn next_pair<S>(
+    half_thickness: S,
+    point_a: &mut Option<Point2<S>>,
+    point_b: &mut Option<Point2<S>>,
+    next_point: Option<Point2<S>>,
+) -> Option<[Point2<S>; 2]>
+where
+    S: BaseFloat,
+{
+    let a = point_a.take();
+    let b = point_b.take();
+    match (a, b) {
+        // Only occurs if there were no points in the sequence or if the last point has already
+        // been yeilded.
+        (None, None) | (Some(_), None) => None,
+
+        // Should only occur on the first iteration.
+        (None, Some(b)) => {
+            let c = match next_point {
+                Some(c) => c,
+                // Only occurs if there was only point, in which case we cannot give valid
+                // vertices.
+                None => return None,
+            };
+            // Get the line quad between the two points.
+            let line = Line { start: b, end: c, half_thickness };
+            let Quad([r, l, _, _]) = line.quad_corners();
+            *point_a = Some(b);
+            *point_b = Some(c);
+            Some([l, r])
+        },
+
+        // Every other point.
+        (Some(a), Some(b)) => {
+            let c = match next_point {
+                Some(c) => c,
+                // If this is the last point.
+                None => {
+                    // Get the line quad between the two points.
+                    let line = Line { start: a, end: b, half_thickness };
+                    let Quad([_, _, l, r]) = line.quad_corners();
+                    *point_a = Some(b);
+                    return Some([l, r]);
+                },
+            };
+            let ab = Line { start: a, end: b, half_thickness };
+            let bc = Line { start: b, end: c, half_thickness };
+            let Quad([ar, al, bl_ab, br_ab]) = ab.quad_corners();
+            let Quad([br_bc, bl_bc, cl, cr]) = bc.quad_corners();
+            let mut il = match line::join::intersect((al, bl_ab), (cl, bl_bc)) {
+                Some(il) => il,
+                // If the lines are parallel, produce the join vertices.
+                None => {
+                    bl_ab
+                },
+            };
+            let mut ir = match line::join::intersect((ar, br_ab), (cr, br_bc)) {
+                Some(ir) => ir,
+                None => {
+                    br_ab
+                },
+            };
+            *point_a = Some(b);
+            *point_b = Some(c);
+            Some([il, ir])
+        },
+    }
+}
+
+impl<I, S> ExactSizeIterator for VertexPairs<I, S>
+where
+    I: Iterator<Item = Point2<S>> + ExactSizeIterator,
+    S: BaseFloat,
+{
+    fn len(&self) -> usize {
+        let remaining_points = self.points.len();
+        let a = self.point_a.is_some();
+        let b = self.point_b.is_some();
         match (a, b) {
-            // Only occurs if there were no points in the given iterator or if the last has already
-            // been yeilded.
-            (None, None) | (Some(_), None) => None,
-
-            // Only occurs on the first iteration.
-            (None, Some(b)) => {
-                let c = match points.next() {
-                    Some(c) => c,
-                    // Only occurs if there was only point, in which case we cannot give valid
-                    // vertices.
-                    None => return None,
-                };
-                // Get the line quad between the two points.
-                let line = Line { start: b, end: c, half_thickness };
-                let Quad([r, l, _, _]) = line.quad_corners();
-                *point_a = Some(b);
-                *point_b = Some(c);
-                Some([l, r])
-            },
-
-            // Every other point.
-            (Some(a), Some(b)) => {
-                let c = match points.next() {
-                    Some(c) => c,
-                    // If this is the last point.
-                    None => {
-                        // Get the line quad between the two points.
-                        let line = Line { start: a, end: b, half_thickness };
-                        let Quad([_, _, l, r]) = line.quad_corners();
-                        *point_a = Some(b);
-                        return Some([l, r]);
-                    },
-                };
-                let ab = Line { start: a, end: b, half_thickness };
-                let bc = Line { start: b, end: c, half_thickness };
-                let Quad([ar, al, bl_ab, br_ab]) = ab.quad_corners();
-                let Quad([br_bc, bl_bc, cl, cr]) = bc.quad_corners();
-                let mut il = match line::join::intersect((al, bl_ab), (cl, bl_bc)) {
-                    Some(il) => il,
-                    // If the lines are parallel, produce the join vertices.
-                    None => {
-                        bl_ab
-                    },
-                };
-                let mut ir = match line::join::intersect((ar, br_ab), (cr, br_bc)) {
-                    Some(ir) => ir,
-                    None => {
-                        br_ab
-                    },
-                };
-                *point_a = Some(b);
-                *point_b = Some(c);
-                Some([il, ir])
-            },
+            (false, true) => if remaining_points <= 1 { 0 } else { remaining_points },
+            (true, true) => remaining_points + 1,
+            _ => 0,
         }
+    }
+}
+
+impl<I, S> Iterator for Vertices<I, S>
+where
+    I: Iterator<Item = Point2<S>>,
+    S: BaseFloat,
+{
+    type Item = Point2<S>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next) = self.next.take() {
+            return Some(next);
+        }
+        if let Some([this, next]) = self.pairs.next() {
+            self.next = Some(next);
+            return Some(this);
+        }
+        None
+    }
+}
+
+impl<I, S> ExactSizeIterator for Vertices<I, S>
+where
+    I: Iterator<Item = Point2<S>> + ExactSizeIterator,
+    S: BaseFloat,
+{
+    fn len(&self) -> usize {
+        self.pairs.len() * 2 + if self.next.is_some() { 1 } else { 0 }
     }
 }
 
@@ -203,13 +289,19 @@ impl Iterator for TriangleIndices {
     }
 }
 
+impl ExactSizeIterator for TriangleIndices {
+    fn len(&self) -> usize {
+        self.n_tris - self.i
+    }
+}
+
 /// Produces an iterator yeilding the miter normal vertices for each point in the given iterator.
-pub fn vertices<I, S>(points: I, half_thickness: S) -> Vertices<I::IntoIter, S>
+pub fn vertices<I, S>(points: I, half_thickness: S) -> VertexPairs<I::IntoIter, S>
 where
     I: IntoIterator<Item = Point2<S>>,
     S: BaseFloat,
 {
-    Vertices::new(points, half_thickness)
+    VertexPairs::new(points, half_thickness)
 }
 
 /// Produce an iterator yielding the indices necessary to triangulate a miter polyline from a
