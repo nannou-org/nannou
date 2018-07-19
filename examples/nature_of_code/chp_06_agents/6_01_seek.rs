d54e2073fe908eee77ce8503783c77376024e33e
Tom Gowan <s3549186@student.rmit.edu.au>
2018-07-19 18:37:45 +1000

    noc

---
+++ examples/nature_of_code/chp_06_agents/6_01_seek.rs
@@ -0,0 +1,10 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Seeking "vehicle" follows the mouse position
+
+// Implements Craig Reynold's autonomous steering behaviors
+// One vehicle "seeks"
+// See: http://www.red3d.com/cwr/
+extern crate nannou;
