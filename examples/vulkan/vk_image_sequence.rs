c1813de1e5789030d95cf7598ef8d060d7ca2881
mitchmindtree <mitchell.nordine@gmail.com>
2018-12-12 14:48:52 +0700

    Tweak and rustfmt examples ready for vulkan branch
    Switches vulkano to freesigs branch that includes two patches.
    Switches vulkano-shaders to the master vulkano branch for `#include`
    support.

--- examples/vulkan/vk_image_sequence.rs
+++ examples/vulkan/vk_image_sequence.rs
@@ -2,29 +2,30 @@ extern crate nannou;
 
 use nannou::prelude::*;
 use nannou::vulkano;
-use std::sync::Arc;
 use std::cell::RefCell;
+use std::sync::Arc;
 
 use nannou::vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use nannou::vulkano::command_buffer::DynamicState;
-use nannou::vulkano::device::DeviceOwned;
-use nannou::vulkano::pipeline::{GraphicsPipeline, GraphicsPipelineAbstract};
-use nannou::vulkano::pipeline::viewport::Viewport;
-use nannou::vulkano::framebuffer::{Framebuffer, FramebufferAbstract, Subpass, RenderPassAbstract, FramebufferCreationError};
-use nannou::vulkano::image::{ImmutableImage, Dimensions};
-use nannou::vulkano::sampler::{Sampler, SamplerAddressMode, Filter, MipmapMode};
-use nannou::vulkano::format::Format;
 use nannou::vulkano::descriptor::descriptor_set::{DescriptorSet, PersistentDescriptorSet};
+use nannou::vulkano::device::DeviceOwned;
+use nannou::vulkano::format::Format;
+use nannou::vulkano::framebuffer::{
+    Framebuffer, FramebufferAbstract, FramebufferCreationError, RenderPassAbstract, Subpass,
+};
+use nannou::vulkano::image::{Dimensions, ImmutableImage};
+use nannou::vulkano::pipeline::viewport::Viewport;
+use nannou::vulkano::pipeline::{GraphicsPipeline, GraphicsPipelineAbstract};
+use nannou::vulkano::sampler::{Filter, MipmapMode, Sampler, SamplerAddressMode};
 
 fn main() {
     nannou::app(model)
-        .event(event) // The function that will be called when the app receives events.
-        .view(view) // The function that will be called for drawing to the window.
+        .event(event)
+        .view(view)
         .run();
 }
 
 struct Model {
-    // Store the window ID so we can refer to this specific window later if needed.
     _window: WindowId,
     render_pass: Arc<RenderPassAbstract + Send + Sync>,
     pipeline: Arc<GraphicsPipelineAbstract + Send + Sync>,
@@ -34,67 +35,69 @@ struct Model {
 }
 
 #[derive(Debug, Clone)]
-struct Vertex { position: [f32; 2] }
+struct Vertex {
+    position: [f32; 2],
+}
+
 nannou::vulkano::impl_vertex!(Vertex, position);
 
 fn model(app: &App) -> Model {
+    let _window = app
+        .new_window()
+        .with_dimensions(220, 220)
+        .with_title("nannou")
+        .build()
+        .unwrap();
 
-    let device = app.vulkan_physical_devices().nth(0).unwrap();
-    println!("name = {:?}", device.name());
-    // Create a new window! Store the ID so we can refer to it later.
-    let _window = app.new_window().with_dimensions(220, 220).vulkan_physical_device(device).with_title("nannou").build().unwrap();
-
-    // The gpu device associated with the window's swapchain
     let device = app.main_window().swapchain().device().clone();
 
-    // We now create a buffer that will store the shape of our triangle.
     let vertex_buffer = CpuAccessibleBuffer::from_iter(
         device.clone(),
         BufferUsage::all(),
         [
-            Vertex { position: [-1.0, -1.0 ] },
-            Vertex { position: [-1.0,  1.0 ] },
-            Vertex { position: [ 1.0, -1.0 ] },
-            Vertex { position: [ 1.0,  1.0 ] },
-        ].iter().cloned()
-    ).unwrap();
+            Vertex {
+                position: [-1.0, -1.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+            },
+        ]
+        .iter()
+        .cloned(),
+    )
+    .unwrap();
 
     let vertex_shader = vs::Shader::load(device.clone()).unwrap();
     let fragment_shader = fs::Shader::load(device.clone()).unwrap();
 
-    // The next step is to create a *render pass*, which is an object that describes where the
-    // output of the graphics pipeline will go. It describes the layout of the images
-    // where the colors, depth and/or stencil information will be written.
-    let render_pass = Arc::new(nannou::vulkano::single_pass_renderpass!(
-        device.clone(),
-        attachments: {
-            // `color` is a custom name we give to the first and only attachment.
-            color: {
-                // `load: Clear` means that we ask the GPU to clear the content of this
-                // attachment at the start of the drawing.
-                load: Clear,
-                // `store: Store` means that we ask the GPU to store the output of the draw
-                // in the actual image. We could also ask it to discard the result.
-                store: Store,
-                // `format: <ty>` indicates the type of the format of the image. This has to
-                // be one of the types of the `vulkano::format` module (or alternatively one
-                // of your structs that implements the `FormatDesc` trait). Here we use the
-                // same format as the swapchain.
-                format: app.main_window().swapchain().format(),
-                // TODO:
-                samples: 1,
-                initial_layout: ImageLayout::PresentSrc,
-                final_layout: ImageLayout::PresentSrc,
+    let render_pass = Arc::new(
+        nannou::vulkano::single_pass_renderpass!(
+            device.clone(),
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: app.main_window().swapchain().format(),
+                    samples: 1,
+                    initial_layout: ImageLayout::PresentSrc,
+                    final_layout: ImageLayout::PresentSrc,
+                }
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {}
             }
-        },
-        pass: {
-            // We use the attachment named `color` as the one and only color attachment.
-            color: [color],
-            // No depth-stencil attachment is indicated with empty brackets.
-            depth_stencil: {}
-        }
-    ).unwrap());
+        )
+        .unwrap(),
+    );
 
+    // Load the images from a directory.
     let (texture, _tex_future) = {
         let sequence_path = app.assets_path().unwrap().join("images").join("sequence");
         let mut images = vec![];
@@ -102,11 +105,13 @@ fn model(app: &App) -> Model {
         for entry in std::fs::read_dir(sequence_path).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
-            if path.to_str().unwrap() == "/Users/joshuabatty/Documents/Software/rust/nannou/assets/images/sequence/.DS_Store" {
-                continue;
-            }
-            //println!("loading image: {}", path.display());
-            let image = image::open(&path).unwrap().to_rgba();
+            let image = match image::open(&path) {
+                Ok(img) => img.to_rgba(),
+                Err(err) => {
+                    eprintln!("failed to open {} as an image: {}", path.display(), err);
+                    continue;
+                }
+            };
             let (w, h) = image.dimensions();
             width = w;
             height = h;
@@ -115,15 +120,21 @@ fn model(app: &App) -> Model {
         }
         images.sort();
         let array_layers = images.len() as u32;
-        let image_data: Vec<_> = images.iter()
+        let image_data: Vec<_> = images
+            .iter()
             .flat_map(|(_, img)| img.iter().cloned())
             .collect();
         ImmutableImage::from_iter(
             image_data.into_iter(),
-            Dimensions::Dim2dArray { width, height, array_layers },
+            Dimensions::Dim2dArray {
+                width,
+                height,
+                array_layers,
+            },
             Format::R8G8B8A8Srgb,
-            app.main_window().queue().clone(),
-        ).unwrap()
+            app.main_window().swapchain_queue().clone(),
+        )
+        .unwrap()
     };
 
     let sampler = Sampler::new(
@@ -138,106 +149,81 @@ fn model(app: &App) -> Model {
         1.0,
         0.0,
         0.0,
-    ).unwrap();
+    )
+    .unwrap();
 
-    // Before we draw we have to create what is called a pipeline. This is similar to an OpenGL
-    // program, but much more specific.
-    let pipeline = Arc::new(GraphicsPipeline::start()
-        // We need to indicate the layout of the vertices.
-        // The type `SingleBufferDefinition` actually contains a template parameter corresponding
-        // to the type of each vertex. But in this code it is automatically inferred.
-        .vertex_input_single_buffer::<Vertex>()
-        // A Vulkan shader can in theory contain multiple entry points, so we have to specify
-        // which one. The `main` word of `main_entry_point` actually corresponds to the name of
-        // the entry point.
-        .vertex_shader(vertex_shader.main_entry_point(), ())
-        // The content of the vertex buffer describes a list of triangles.
-        .triangle_strip()
-        // Use a resizable viewport set to draw over the entire window
-        .viewports_dynamic_scissors_irrelevant(1)
-        // See `vertex_shader`.
-        .fragment_shader(fragment_shader.main_entry_point(), ())
-        // Enable Alpha Blending
-        .blend_alpha_blending()
-        // We have to indicate which subpass of which render pass this pipeline is going to be used
-        // in. The pipeline will only be usable from this particular subpass.
-        .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
-        // Now that our builder is filled, we call `build()` to obtain an actual pipeline.
-        .build(device.clone())
-        .unwrap());
-
+    let pipeline = Arc::new(
+        GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vertex_shader.main_entry_point(), ())
+            .triangle_strip()
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fragment_shader.main_entry_point(), ())
+            .blend_alpha_blending()
+            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
 
     let desciptor_set = Arc::new(
         PersistentDescriptorSet::start(pipeline.clone(), 0)
             .add_sampled_image(texture.clone(), sampler.clone())
             .unwrap()
             .build()
-            .unwrap()
+            .unwrap(),
     );
 
-    // The render pass we created above only describes the layout of our framebuffers. Before we
-    // can draw we also need to create the actual framebuffers.
-    //
-    // Since we need to draw to multiple images, we are going to create a different framebuffer for
-    // each image.
-    let framebuffers = RefCell::new(Vec::new());    
+    let framebuffers = RefCell::new(Vec::new());
 
-    Model { _window, render_pass, pipeline, vertex_buffer, framebuffers, desciptor_set }
+    Model {
+        _window,
+        render_pass,
+        pipeline,
+        vertex_buffer,
+        framebuffers,
+        desciptor_set,
+    }
 }
 
-// Handle events related to the window and update the model if necessary
-fn event(app: &App, model: Model, event: Event) -> Model {
-    if let Event::Update(_update) = event {
-    }
+fn event(_app: &App, model: Model, _event: Event) -> Model {
     model
 }
 
-// Draw the state of your `Model` into the given `Frame` here.
 fn view(app: &App, model: &Model, frame: Frame) -> Frame {
-    // Clear the window with a "dark charcoal" shade.
-    frame.clear(BLUE);
-
-    // Dynamic viewports allow us to recreate just the viewport when the window is resized
-    // Otherwise we would have to recreate the whole pipeline.
     let [w, h] = frame.swapchain_image().dimensions();
     let viewport = Viewport {
         origin: [0.0, 0.0],
         dimensions: [w as _, h as _],
-        depth_range: 0.0 .. 1.0,
+        depth_range: 0.0..1.0,
     };
     let dynamic_state = DynamicState {
         line_width: None,
         viewports: Some(vec![viewport]),
-        scissors: None 
+        scissors: None,
     };
 
     // Update the framebuffers if necessary.
     while frame.swapchain_image_index() >= model.framebuffers.borrow().len() {
-        let fb = create_framebuffer(
-            model.render_pass.clone(),
-            frame.swapchain_image().clone(),
-        ).unwrap();
+        let fb =
+            create_framebuffer(model.render_pass.clone(), frame.swapchain_image().clone()).unwrap();
         model.framebuffers.borrow_mut().push(Arc::new(fb));
     }
 
     // If the dimensions for the current framebuffer do not match, recreate it.
     if frame.swapchain_image_is_new() {
         let fb = &mut model.framebuffers.borrow_mut()[frame.swapchain_image_index()];
-        let new_fb = create_framebuffer(
-            model.render_pass.clone(),
-            frame.swapchain_image().clone(),
-        ).unwrap();
+        let new_fb =
+            create_framebuffer(model.render_pass.clone(), frame.swapchain_image().clone()).unwrap();
         *fb = Arc::new(new_fb);
     }
 
-    // Specify the color to clear the framebuffer with i.e. blue
-    let clear_values = vec!([0.0, 1.0, 0.0, 1.0].into());
+    let clear_values = vec![[0.0, 1.0, 0.0, 1.0].into()];
+
     let push_constants = fs::ty::PushConstantData {
         sequence_idx: (app.time * 124.0) as i32 % 86,
         time: app.time * 20.0,
     };
 
-    // Submit the draw commands.
     frame
         .add_commands()
         .begin_render_pass(
@@ -263,16 +249,14 @@ fn view(app: &App, model: &Model, frame: Frame) -> Frame {
     draw.ellipse()
         .x_y(app.mouse.x * t.cos(), app.mouse.y)
         .radius(win.w() * 0.125 * t.sin())
-        .rgba(1.0,0.0,0.0,0.4);
-    //draw.to_frame(app, &frame).unwrap();
+        .rgba(1.0, 0.0, 0.0, 0.4);
 
-    // Return the cleared frame.
     frame
 }
 
 mod vs {
-    nannou::vulkano_shaders::shader!{
-        ty: "vertex",
+    nannou::vulkano_shaders::shader! {
+    ty: "vertex",
         src: "
 #version 450
 
@@ -287,8 +271,8 @@ void main() {
 }
 
 mod fs {
-    nannou::vulkano_shaders::shader!{
-        ty: "fragment",
+    nannou::vulkano_shaders::shader! {
+    ty: "fragment",
         src: "
 #version 450
 
@@ -308,6 +292,7 @@ void main() {
 }"
     }
 }
+
 // Create the framebuffer for the image.
 fn create_framebuffer(
     render_pass: Arc<RenderPassAbstract + Send + Sync>,
--- examples/vulkan/vk_shader_include/mod.rs
+++ examples/vulkan/vk_shader_include/mod.rs
@@ -2,25 +2,26 @@ extern crate nannou;
 
 use nannou::prelude::*;
 use nannou::vulkano;
-use std::sync::Arc;
 use std::cell::RefCell;
+use std::sync::Arc;
 
 use nannou::vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use nannou::vulkano::command_buffer::DynamicState;
 use nannou::vulkano::device::DeviceOwned;
-use nannou::vulkano::pipeline::{GraphicsPipeline, GraphicsPipelineAbstract};
+use nannou::vulkano::framebuffer::{
+    Framebuffer, FramebufferAbstract, FramebufferCreationError, RenderPassAbstract, Subpass,
+};
 use nannou::vulkano::pipeline::viewport::Viewport;
-use nannou::vulkano::framebuffer::{Framebuffer, FramebufferAbstract, Subpass, RenderPassAbstract, FramebufferCreationError};
+use nannou::vulkano::pipeline::{GraphicsPipeline, GraphicsPipelineAbstract};
 
 fn main() {
     nannou::app(model)
-        .event(event) // The function that will be called when the app receives events.
-        .view(view) // The function that will be called for drawing to the window.
+        .event(event)
+        .view(view)
         .run();
 }
 
 struct Model {
-    // Store the window ID so we can refer to this specific window later if needed.
     _window: WindowId,
     render_pass: Arc<RenderPassAbstract + Send + Sync>,
     pipeline: Arc<GraphicsPipelineAbstract + Send + Sync>,
@@ -29,147 +30,125 @@ struct Model {
 }
 
 #[derive(Debug, Clone)]
-struct Vertex { position: [f32; 2] }
+struct Vertex {
+    position: [f32; 2],
+}
+
 nannou::vulkano::impl_vertex!(Vertex, position);
 
 fn model(app: &App) -> Model {
-    // Create a new window! Store the ID so we can refer to it later.
-    let _window = app.new_window().with_dimensions(1024, 512).with_title("nannou").build().unwrap();
+    let _window = app
+        .new_window()
+        .with_dimensions(1024, 512)
+        .with_title("nannou")
+        .build()
+        .unwrap();
 
-    // The gpu device associated with the window's swapchain
     let device = app.main_window().swapchain().device().clone();
 
-    // We now create a buffer that will store the shape of our triangle.
     let vertex_buffer = CpuAccessibleBuffer::from_iter(
         device.clone(),
         BufferUsage::all(),
         [
-            Vertex { position: [-1.0, -1.0 ] },
-            Vertex { position: [-1.0,  1.0 ] },
-            Vertex { position: [ 1.0, -1.0 ] },
-            Vertex { position: [ 1.0,  1.0 ] },
-        ].iter().cloned()
-    ).unwrap();
+            Vertex {
+                position: [-1.0, -1.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+            },
+        ]
+        .iter()
+        .cloned(),
+    )
+    .unwrap();
 
     let vertex_shader = vs::Shader::load(device.clone()).unwrap();
     let fragment_shader = fs::Shader::load(device.clone()).unwrap();
 
-    // The next step is to create a *render pass*, which is an object that describes where the
-    // output of the graphics pipeline will go. It describes the layout of the images
-    // where the colors, depth and/or stencil information will be written.
-    let render_pass = Arc::new(nannou::vulkano::single_pass_renderpass!(
-        device.clone(),
-        attachments: {
-            // `color` is a custom name we give to the first and only attachment.
-            color: {
-                // `load: Clear` means that we ask the GPU to clear the content of this
-                // attachment at the start of the drawing.
-                load: Clear,
-                // `store: Store` means that we ask the GPU to store the output of the draw
-                // in the actual image. We could also ask it to discard the result.
-                store: Store,
-                // `format: <ty>` indicates the type of the format of the image. This has to
-                // be one of the types of the `vulkano::format` module (or alternatively one
-                // of your structs that implements the `FormatDesc` trait). Here we use the
-                // same format as the swapchain.
-                format: app.main_window().swapchain().format(),
-                // TODO:
-                samples: 1,
-                initial_layout: ImageLayout::PresentSrc,
-                final_layout: ImageLayout::PresentSrc,
+    let render_pass = Arc::new(
+        nannou::vulkano::single_pass_renderpass!(
+            device.clone(),
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: app.main_window().swapchain().format(),
+                    samples: 1,
+                    initial_layout: ImageLayout::PresentSrc,
+                    final_layout: ImageLayout::PresentSrc,
+                }
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {}
             }
-        },
-        pass: {
-            // We use the attachment named `color` as the one and only color attachment.
-            color: [color],
-            // No depth-stencil attachment is indicated with empty brackets.
-            depth_stencil: {}
-        }
-    ).unwrap());
+        )
+        .unwrap(),
+    );
 
-    // Before we draw we have to create what is called a pipeline. This is similar to an OpenGL
-    // program, but much more specific.
-    let pipeline = Arc::new(GraphicsPipeline::start()
-        // We need to indicate the layout of the vertices.
-        // The type `SingleBufferDefinition` actually contains a template parameter corresponding
-        // to the type of each vertex. But in this code it is automatically inferred.
-        .vertex_input_single_buffer::<Vertex>()
-        // A Vulkan shader can in theory contain multiple entry points, so we have to specify
-        // which one. The `main` word of `main_entry_point` actually corresponds to the name of
-        // the entry point.
-        .vertex_shader(vertex_shader.main_entry_point(), ())
-        // The content of the vertex buffer describes a list of triangles.
-        .triangle_strip()
-        // Use a resizable viewport set to draw over the entire window
-        .viewports_dynamic_scissors_irrelevant(1)
-        // See `vertex_shader`.
-        .fragment_shader(fragment_shader.main_entry_point(), ())
-        // Enable Alpha Blending
-        .blend_alpha_blending()
-        // We have to indicate which subpass of which render pass this pipeline is going to be used
-        // in. The pipeline will only be usable from this particular subpass.
-        .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
-        // Now that our builder is filled, we call `build()` to obtain an actual pipeline.
-        .build(device.clone())
-        .unwrap());
+    let pipeline = Arc::new(
+        GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vertex_shader.main_entry_point(), ())
+            .triangle_strip()
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fragment_shader.main_entry_point(), ())
+            .blend_alpha_blending()
+            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
 
-    // The render pass we created above only describes the layout of our framebuffers. Before we
-    // can draw we also need to create the actual framebuffers.
-    //
-    // Since we need to draw to multiple images, we are going to create a different framebuffer for
-    // each image.
-    let framebuffers = RefCell::new(Vec::new());    
+    let framebuffers = RefCell::new(Vec::new());
 
-    Model { _window, render_pass, pipeline, vertex_buffer, framebuffers }
+    Model {
+        _window,
+        render_pass,
+        pipeline,
+        vertex_buffer,
+        framebuffers,
+    }
 }
 
-// Handle events related to the window and update the model if necessary
-fn event(_app: &App, model: Model, event: Event) -> Model {
-    if let Event::Update(_update) = event {
-    }
+fn event(_app: &App, model: Model, _event: Event) -> Model {
     model
 }
 
-// Draw the state of your `Model` into the given `Frame` here.
 fn view(app: &App, model: &Model, frame: Frame) -> Frame {
-    // Clear the window with a "dark charcoal" shade.
-    frame.clear(BLUE);
-
-    // Dynamic viewports allow us to recreate just the viewport when the window is resized
-    // Otherwise we would have to recreate the whole pipeline.
     let [w, h] = frame.swapchain_image().dimensions();
     let viewport = Viewport {
         origin: [0.0, 0.0],
         dimensions: [w as _, h as _],
-        depth_range: 0.0 .. 1.0,
+        depth_range: 0.0..1.0,
     };
     let dynamic_state = DynamicState {
         line_width: None,
         viewports: Some(vec![viewport]),
-        scissors: None 
+        scissors: None,
     };
 
     // Update the framebuffers if necessary.
     while frame.swapchain_image_index() >= model.framebuffers.borrow().len() {
-        let fb = create_framebuffer(
-            model.render_pass.clone(),
-            frame.swapchain_image().clone(),
-        ).unwrap();
+        let fb =
+            create_framebuffer(model.render_pass.clone(), frame.swapchain_image().clone()).unwrap();
         model.framebuffers.borrow_mut().push(Arc::new(fb));
     }
 
     // If the dimensions for the current framebuffer do not match, recreate it.
     if frame.swapchain_image_is_new() {
         let fb = &mut model.framebuffers.borrow_mut()[frame.swapchain_image_index()];
-        let new_fb = create_framebuffer(
-            model.render_pass.clone(),
-            frame.swapchain_image().clone(),
-        ).unwrap();
+        let new_fb =
+            create_framebuffer(model.render_pass.clone(), frame.swapchain_image().clone()).unwrap();
         *fb = Arc::new(new_fb);
     }
 
-    // Specify the color to clear the framebuffer with i.e. blue
-    let clear_values = vec!([0.0, 1.0, 0.0, 1.0].into());
+    let clear_values = vec![[0.0, 1.0, 0.0, 1.0].into()];
 
     let push_constants = fs::ty::PushConstantData {
         time: app.time,
@@ -177,7 +156,6 @@ fn view(app: &App, model: &Model, frame: Frame) -> Frame {
         height: h as f32,
     };
 
-    // Submit the draw commands.
     frame
         .add_commands()
         .begin_render_pass(
@@ -197,13 +175,12 @@ fn view(app: &App, model: &Model, frame: Frame) -> Frame {
         .end_render_pass()
         .expect("failed to add `end_render_pass` command");
 
-    // Return the cleared frame.
     frame
 }
 
 mod vs {
-    nannou::vulkano_shaders::shader!{
-        ty: "vertex",
+    nannou::vulkano_shaders::shader! {
+    ty: "vertex",
         src: "
 #version 450
 
@@ -218,11 +195,11 @@ void main() {
 }
 
 mod fs {
-    nannou::vulkano_shaders::shader!{
-        ty: "fragment",
-        // We declare what directories to search for when using the `#include <...>`
-        // syntax. Specified directories have descending priorities based on their order.
-        include: [ "examples/vulkan/vk_shader_include/common_shaders" ],
+    nannou::vulkano_shaders::shader! {
+    ty: "fragment",
+    // We declare what directories to search for when using the `#include <...>`
+    // syntax. Specified directories have descending priorities based on their order.
+    include: [ "examples/vulkan/vk_shader_include/common_shaders" ],
         src: "
 #version 450
 // Substitutes this line with the contents of the file `lfos.glsl` found in one of the standard
@@ -268,7 +245,7 @@ void main() {
 }"
     }
 }
-// Create the framebuffer for the image.
+
 fn create_framebuffer(
     render_pass: Arc<RenderPassAbstract + Send + Sync>,
     swapchain_image: Arc<nannou::window::SwapchainImage>,
