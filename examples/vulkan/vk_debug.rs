02e2b2eb5abe624cd937fe146d294ed3ccd091b0
mitchmindtree <mitchell.nordine@gmail.com>
2018-12-14 16:00:44 +0700

    Add simple vulkan debug example

---
+++ examples/vulkan/vk_debug.rs
@@ -0,0 +1,32 @@
+//! Use the `nannou::app::Builder::vulkan_debug_callback` method to specify a callback.
+//!
+//! Use `Default::default()` to simply use the default callback which will enable all message types
+//! including error, warning, performance_warning, information and debug. See the
+//! `nannou::gpu::VulkanDebugCallbackBuilder` docs for more information on how to customise this.
+//!
+//! If you require specifying custom validation layers, please see
+//! `nannou::app::Builder::vulkan_instance` and the `nannou::gpu::VulkanInstanceBuilder` which will
+//! allow you to specify your own custom set of validation layers. To determine what layers are
+//! available on a system, see the `nannou::vulkano::instance::layers_list` function.
+
+extern crate nannou;
+
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .vulkan_debug_callback(Default::default()) // The vulkan debug callback.
+        .simple_window(view)
+        .run();
+}
+
+struct Model;
+
+fn model(_app: &App) -> Model {
+    Model
+}
+
+fn view(_app: &App, _model: &Model, frame: Frame) -> Frame {
+    frame.clear(DARK_BLUE);
+    frame
+}
