1436533cd4a6dcf6d9aab67dd5ee65ae265e7e3a
mitchmindtree <mitchell.nordine@gmail.com>
2019-04-28 15:21:19 +1000

    Connect to all detected DACs in `laser_frame_stream_gui` example
    Rather than just the first.

--- examples/laser/laser_frame_stream_gui.rs
+++ examples/laser/laser_frame_stream_gui.rs
@@ -7,36 +7,45 @@ extern crate nannou;
 use nannou::geom::Rect;
 use nannou::prelude::*;
 use nannou::ui::prelude::*;
+use std::sync::{mpsc, Arc};
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
 struct Model {
-    laser_api: lasy::Lasy,
-    laser_stream: lasy::FrameStream<Laser>,
+    // A handle to the laser API used for spawning streams and detecting DACs.
+    laser_api: Arc<lasy::Lasy>,
+    // All of the live stream handles.
+    laser_streams: Vec<lasy::FrameStream<Laser>>,
+    // A copy of the state that will live on the laser thread so we can present a GUI.
+    laser_model: Laser,
+    // A copy of the laser settings so that we can control them with the GUI.
     laser_settings: LaserSettings,
+    // For receiving newly detected DACs.
+    dac_rx: mpsc::Receiver<lasy::DetectedDac>,
+    // The UI for control over laser parameters and settings.
     ui: Ui,
+    // The unique ID for each UI widget.
     ids: Ids,
 }
 
+#[derive(Clone)]
 struct Laser {
     draw_mode: DrawMode,
+    scale: f32,
     color_profile: RgbProfile,
     point_weight: u32,
     test_pattern: TestPattern,
 }
 
 struct LaserSettings {
-    draw_mode: DrawMode,
-    point_weight: u32,
     point_hz: u32,
     latency_points: u32,
     frame_hz: u32,
     distance_per_point: f32,
     blank_delay_points: u32,
     radians_per_point: f32,
-    color_profile: RgbProfile,
 }
 
 #[derive(Clone, Copy)]
@@ -50,6 +59,7 @@ struct RgbProfile {
 enum DrawMode { Lines, Points }
 
 // A collection of laser test patterns. We'll toggle between these with the numeric keys.
+#[derive(Copy, Clone)]
 pub enum TestPattern {
     // A rectangle that outlines the laser's entire field of projection.
     Rectangle,
@@ -71,6 +81,7 @@ struct Ids {
     draw_mode_lines_button: widget::Id,
     draw_mode_points_button: widget::Id,
     point_weight_slider: widget::Id,
+    scale_slider: widget::Id,
     laser_settings_text: widget::Id,
     point_hz_slider: widget::Id,
     latency_points_slider: widget::Id,
@@ -85,20 +96,29 @@ struct Ids {
     blue_slider: widget::Id,
 }
 
+impl Default for Laser {
+    fn default() -> Self {
+        Laser {
+            draw_mode: DrawMode::Lines,
+            scale: 1.0,
+            point_weight: lasy::Point::DEFAULT_LINE_POINT_WEIGHT,
+            test_pattern: TestPattern::Rectangle,
+            color_profile: Default::default(),
+        }
+    }
+}
+
 impl Default for LaserSettings {
     fn default() -> Self {
         use lasy::stream;
         use lasy::stream::frame::opt::InterpolationConfig;
         LaserSettings {
-            draw_mode: DrawMode::Lines,
-            point_weight: lasy::Point::DEFAULT_LINE_POINT_WEIGHT,
             point_hz: stream::DEFAULT_POINT_HZ,
             latency_points: stream::points_per_frame(stream::DEFAULT_POINT_HZ, stream::DEFAULT_FRAME_HZ),
             frame_hz: stream::DEFAULT_FRAME_HZ,
             distance_per_point: InterpolationConfig::DEFAULT_DISTANCE_PER_POINT,
             blank_delay_points: InterpolationConfig::DEFAULT_BLANK_DELAY_POINTS,
             radians_per_point: InterpolationConfig::DEFAULT_RADIANS_PER_POINT,
-            color_profile: Default::default(),
         }
     }
 }
@@ -124,14 +144,31 @@ fn model(app: &App) -> Model {
 
     // Initialise the state that we want to live on the laser thread and spawn the stream.
     let laser_settings = LaserSettings::default();
-    let laser_model = Laser {
-        draw_mode: laser_settings.draw_mode,
-        point_weight: laser_settings.point_weight,
-        color_profile: laser_settings.color_profile,
-        test_pattern: TestPattern::Rectangle,
-    };
-    let laser_api = lasy::Lasy::new();
-    let laser_stream = laser_api.new_frame_stream(laser_model, laser).build().unwrap();
+    let laser_model = Laser::default();
+
+    // TODO Implement `Clone` for `Lasy` so that we don't have to `Arc` it.
+    let laser_api = Arc::new(lasy::Lasy::new());
+
+    // A channel for receiving newly detected DACs.
+    let (dac_tx, dac_rx) = mpsc::channel();
+
+    // Spawn a thread for detecting the DACs.
+    let laser_api2 = laser_api.clone();
+    std::thread::spawn(move || {
+        let mut detected = std::collections::HashSet::new();
+        for res in laser_api2.detect_dacs().expect("failed to start detecting DACs") {
+            let dac = res.expect("error occurred during DAC detection");
+            if detected.insert(dac.id()) {
+                println!("{:#?}", dac);
+                if dac_tx.send(dac).is_err() {
+                    break;
+                }
+            }
+        }
+    });
+
+    // We'll use a `Vec` to collect laser streams as they appear.
+    let laser_streams = vec![];
 
     // A user-interface to tweak the settings.
     let mut ui = app.new_ui().build().unwrap();
@@ -141,6 +178,7 @@ fn model(app: &App) -> Model {
         draw_mode_lines_button: ui.generate_widget_id(),
         draw_mode_points_button: ui.generate_widget_id(),
         point_weight_slider: ui.generate_widget_id(),
+        scale_slider: ui.generate_widget_id(),
         laser_settings_text: ui.generate_widget_id(),
         point_hz_slider: ui.generate_widget_id(),
         latency_points_slider: ui.generate_widget_id(),
@@ -157,19 +195,27 @@ fn model(app: &App) -> Model {
 
     Model {
         laser_api,
-        laser_stream,
         laser_settings,
+        laser_model,
+        laser_streams,
+        dac_rx,
         ui,
         ids,
     }
 }
 
 // Draw lines or points based on the `DrawMode`.
-fn add_points<I>(points: I, mode: DrawMode, frame: &mut lasy::Frame)
+fn add_points<I>(points: I, mode: DrawMode, scale: f32, frame: &mut lasy::Frame)
 where
     I: IntoIterator,
     I::Item: AsRef<lasy::Point>,
 {
+    let points = points.into_iter().map(|p| {
+        let mut p = p.as_ref().clone();
+        p.position[0] *= scale;
+        p.position[1] *= scale;
+        p
+    });
     match mode {
         DrawMode::Lines => frame.add_lines(points),
         DrawMode::Points => frame.add_points(points),
@@ -195,7 +241,7 @@ fn laser(laser: &mut Laser, frame: &mut lasy::Frame) {
             let bl = [-1.0, -1.0];
             let positions = [tl, tr, br, bl, tl];
             let points = positions.iter().cloned().map(lit_p);
-            add_points(points, laser.draw_mode, frame);
+            add_points(points, laser.draw_mode, laser.scale, frame);
         }
 
         TestPattern::Triangle => {
@@ -204,7 +250,7 @@ fn laser(laser: &mut Laser, frame: &mut lasy::Frame) {
             let c = [0.75, -0.75];
             let positions = [a, b, c, a];
             let points = positions.iter().cloned().map(lit_p);
-            add_points(points, laser.draw_mode, frame);
+            add_points(points, laser.draw_mode, laser.scale, frame);
         }
 
         TestPattern::Crosshair => {
@@ -214,8 +260,8 @@ fn laser(laser: &mut Laser, frame: &mut lasy::Frame) {
             let yb = [0.0, 1.0];
             let x = [lit_p(xa), lit_p(xb)];
             let y = [lit_p(ya), lit_p(yb)];
-            add_points(&x, laser.draw_mode, frame);
-            add_points(&y, laser.draw_mode, frame);
+            add_points(&x, laser.draw_mode, laser.scale, frame);
+            add_points(&y, laser.draw_mode, laser.scale, frame);
         }
 
         TestPattern::ThreeVerticalLines => {
@@ -228,18 +274,18 @@ fn laser(laser: &mut Laser, frame: &mut lasy::Frame) {
             let l = [lit_p(la), lit_p(lb)];
             let m = [lit_p(ma), lit_p(mb)];
             let r = [lit_p(ra), lit_p(rb)];
-            add_points(&l, laser.draw_mode, frame);
-            add_points(&m, laser.draw_mode, frame);
-            add_points(&r, laser.draw_mode, frame);
+            add_points(&l, laser.draw_mode, laser.scale, frame);
+            add_points(&m, laser.draw_mode, laser.scale, frame);
+            add_points(&r, laser.draw_mode, laser.scale, frame);
         }
 
         TestPattern::Circle => {
             let n_points = frame.points_per_frame() as usize / 4;
-            let rect = Rect::from_w_h(1.0, 1.0);
+            let rect = Rect::from_w_h(2.0, 2.0);
             let ellipse: Vec<_> = geom::ellipse::Circumference::new(rect, n_points)
                 .map(|p| lit_p([p.x, p.y]))
                 .collect();
-            add_points(&ellipse, laser.draw_mode, frame);
+            add_points(&ellipse, laser.draw_mode, laser.scale, frame);
         }
 
         TestPattern::Spiral => {
@@ -257,12 +303,23 @@ fn laser(laser: &mut Laser, frame: &mut lasy::Frame) {
                 })
                 .map(lit_p)
                 .collect::<Vec<_>>();
-            add_points(&points, laser.draw_mode, frame);
+            add_points(&points, laser.draw_mode, laser.scale, frame);
         }
     }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
+    // First, check for new laser DACs.
+    for dac in model.dac_rx.try_recv() {
+        println!("Detected DAC {:?}!", dac.id());
+        let stream = model.laser_api
+            .new_frame_stream(model.laser_model.clone(), laser)
+            .detected_dac(dac)
+            .build()
+            .expect("failed to establish stream with newly detected DAC");
+        model.laser_streams.push(stream);
+    }
+
     // Calling `set_widgets` allows us to instantiate some widgets.
     let ui = &mut model.ui.set_widgets();
 
@@ -294,7 +351,7 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.laser_points_text, ui);
 
     // Button colours.
-    let (lines_color, points_color) = match model.laser_settings.draw_mode {
+    let (lines_color, points_color) = match model.laser_model.draw_mode {
         DrawMode::Lines => (color::BLUE, color::BLACK),
         DrawMode::Points => (color::BLACK, color::BLUE),
     };
@@ -306,8 +363,10 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.draw_mode_lines_button, ui)
     {
         let mode = DrawMode::Lines;
-        model.laser_settings.draw_mode = mode;
-        model.laser_stream.send(move |laser| laser.draw_mode = mode).unwrap();
+        model.laser_model.draw_mode = mode;
+        for stream in &model.laser_streams {
+            stream.send(move |laser| laser.draw_mode = mode).ok();
+        }
     }
 
     for _click in button()
@@ -317,18 +376,33 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.draw_mode_points_button, ui)
     {
         let mode = DrawMode::Points;
-        model.laser_settings.draw_mode = mode;
-        model.laser_stream.send(move |laser| laser.draw_mode = mode).unwrap();
+        model.laser_model.draw_mode = mode;
+        for stream in &model.laser_streams {
+            stream.send(move |laser| laser.draw_mode = mode).ok();
+        }
     }
 
-    let label = format!("Point Weight: {}", model.laser_settings.point_weight);
-    for value in slider(model.laser_settings.point_weight as _, 0.0, 128.0)
+    let label = format!("Scale: {}", model.laser_model.scale);
+    for value in slider(model.laser_model.scale as _, 0.0, 1.0)
         .down_from(model.ids.draw_mode_lines_button, 10.0)
+        .label(&label)
+        .set(model.ids.scale_slider, ui)
+    {
+        model.laser_model.scale = value as _;
+        for stream in &model.laser_streams {
+            stream.send(move |laser| laser.scale = value as _).ok();
+        }
+    }
+
+    let label = format!("Point Weight: {}", model.laser_model.point_weight);
+    for value in slider(model.laser_model.point_weight as _, 0.0, 128.0)
         .label(&label)
         .set(model.ids.point_weight_slider, ui)
     {
-        model.laser_settings.point_weight = value as _;
-        model.laser_stream.send(move |laser| laser.point_weight = value as _).unwrap();
+        model.laser_model.point_weight = value as _;
+        for stream in &model.laser_streams {
+            stream.send(move |laser| laser.point_weight = value as _).ok();
+        }
     }
 
     widget::Text::new("Laser Settings")
@@ -343,7 +417,9 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.point_hz_slider, ui)
     {
         model.laser_settings.point_hz = value as _;
-        model.laser_stream.set_point_hz(value as _).unwrap();
+        for stream in &model.laser_streams {
+            stream.set_point_hz(value as _).ok();
+        }
     }
 
     let label = format!("Latency: {} points", model.laser_settings.latency_points);
@@ -352,7 +428,9 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.latency_points_slider, ui)
     {
         model.laser_settings.latency_points = value as _;
-        model.laser_stream.set_latency_points(value as _).unwrap();
+        for stream in &model.laser_streams {
+            stream.set_latency_points(value as _).ok();
+        }
     }
 
     let label = format!("Target FPS: {}", model.laser_settings.frame_hz);
@@ -361,7 +439,9 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.frame_hz_slider, ui)
     {
         model.laser_settings.frame_hz = value as _;
-        model.laser_stream.set_frame_hz(value as _).unwrap();
+        for stream in &model.laser_streams {
+            stream.set_frame_hz(value as _).ok();
+        }
     }
 
     widget::Text::new("Laser Path Interpolation")
@@ -377,7 +457,9 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.distance_per_point_slider, ui)
     {
         model.laser_settings.distance_per_point = value;
-        model.laser_stream.set_distance_per_point(value).unwrap();
+        for stream in &model.laser_streams {
+            stream.set_distance_per_point(value).ok();
+        }
     }
 
     let label = format!("Blank delay: {} points", model.laser_settings.blank_delay_points);
@@ -386,7 +468,9 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .set(model.ids.blank_delay_points_slider, ui)
     {
         model.laser_settings.blank_delay_points = value as _;
-        model.laser_stream.set_blank_delay_points(value as _).unwrap();
+        for stream in &model.laser_streams {
+            stream.set_blank_delay_points(value as _).ok();
+        }
     }
 
     let degrees_per_point = rad_to_deg(model.laser_settings.radians_per_point);
@@ -397,7 +481,9 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
     {
         let radians = deg_to_rad(value);
         model.laser_settings.radians_per_point = radians;
-        model.laser_stream.set_radians_per_point(radians).unwrap();
+        for stream in &model.laser_streams {
+            stream.set_radians_per_point(radians).ok();
+        }
     }
 
     widget::Text::new("Color Profile")
@@ -406,29 +492,35 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         .font_size(16)
         .set(model.ids.color_profile_text, ui);
 
-    for value in slider(model.laser_settings.color_profile.red, 0.0, 1.0)
+    for value in slider(model.laser_model.color_profile.red, 0.0, 1.0)
         .down(20.0)
         .color(color::RED)
         .set(model.ids.red_slider, ui)
     {
-        model.laser_settings.color_profile.red = value;
-        model.laser_stream.send(move |model| model.color_profile.red = value).unwrap();
+        model.laser_model.color_profile.red = value;
+        for stream in &model.laser_streams {
+            stream.send(move |model| model.color_profile.red = value).ok();
+        }
     }
 
-    for value in slider(model.laser_settings.color_profile.green, 0.0, 1.0)
+    for value in slider(model.laser_model.color_profile.green, 0.0, 1.0)
         .color(color::GREEN)
         .set(model.ids.green_slider, ui)
     {
-        model.laser_settings.color_profile.green = value;
-        model.laser_stream.send(move |model| model.color_profile.green = value).unwrap();
+        model.laser_model.color_profile.green = value;
+        for stream in &model.laser_streams {
+            stream.send(move |model| model.color_profile.green = value).ok();
+        }
     }
 
-    for value in slider(model.laser_settings.color_profile.blue, 0.0, 1.0)
+    for value in slider(model.laser_model.color_profile.blue, 0.0, 1.0)
         .color(color::BLUE)
         .set(model.ids.blue_slider, ui)
     {
-        model.laser_settings.color_profile.blue = value;
-        model.laser_stream.send(move |model| model.color_profile.blue = value).unwrap();
+        model.laser_model.color_profile.blue = value;
+        for stream in &model.laser_streams {
+            stream.send(move |model| model.color_profile.blue = value).ok();
+        }
     }
 }
 
@@ -443,7 +535,9 @@ fn key_pressed(_app: &App, model: &mut Model, key: Key) {
         Key::Key6 => TestPattern::Spiral,
         _ => return,
     };
-    model.laser_stream.send(|laser| laser.test_pattern = new_pattern).unwrap();
+    for stream in &model.laser_streams {
+        stream.send(move |laser| laser.test_pattern = new_pattern).ok();
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) -> Frame {
