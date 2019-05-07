847d57803834b7b02769c1aa2fb2f501a9764536
Tom Gowan <s3549186@student.rmit.edu.au>
2019-05-07 14:45:35 +1000

    audio buffer

--- examples/vulkan/vk_hottoy.rs
+++ examples/vulkan/vk_hottoy.rs
@@ -1,8 +1,13 @@
+use nannou::audio::Buffer;
 use nannou::prelude::*;
 use std::cell::RefCell;
 use std::ffi::CStr;
+use std::path::PathBuf;
+use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::Arc;
 use std::time::Duration;
+use std::iter::Cycle;
+use std::ops::Range;
 
 struct Model {
     render_pass: Arc<vk::RenderPassAbstract + Send + Sync>,
@@ -14,8 +19,30 @@ struct Model {
     vert_shader: Arc<vk::pipeline::shader::ShaderModule>,
     frag_shader: Arc<vk::pipeline::shader::ShaderModule>,
     device: Arc<vk::Device>,
-    pool: Option<Arc<RefCell<vk::FixedSizeDescriptorSetsPool<Arc<vk::GraphicsPipelineAbstract + Send + Sync>>>>>,
+    pool: Option<
+        Arc<
+            RefCell<
+                vk::FixedSizeDescriptorSetsPool<Arc<vk::GraphicsPipelineAbstract + Send + Sync>>,
+            >,
+        >,
+    >,
     buffer_pool: vk::CpuBufferPool<UniformData>,
+    audio_buffer_pool: vk::CpuBufferPool<[u8; 4]>,
+    shader_info: ShaderInfo,
+    channel_data: ChannelData,
+    _audio_stream: audio::Stream<Audio>,
+    audio_data: Receiver<[u8; 64]>,
+    audio_buffer: Vec<[u8; 4]>,
+    audio_index: Cycle<Range<usize>>,
+}
+
+struct Audio {
+    tx: Sender<[u8; 64]>,
+    buf: [u8; 64],
+}
+
+struct ShaderInfo {
+    time_delta: f32,
 }
 
 #[derive(Debug, Clone)]
@@ -26,6 +53,9 @@ struct Vertex {
 #[derive(Debug, Clone)]
 struct PushConstantData {
     time: f32,
+    time_delta: f32,
+    frame: u32,
+    frame_rate: u32,
 }
 
 #[derive(Debug, Clone)]
@@ -34,6 +64,12 @@ struct UniformData {
 }
 
 vk::impl_vertex!(Vertex, position);
+
+struct ChannelData {
+    static_channels: Vec<Arc<vk::ImmutableImage<vk::Format>>>,
+    dynamic_channels: Vec<Arc<vk::AttachmentImage<vk::Format>>>,
+    samplers: Vec<Arc<vk::Sampler>>,
+}
 
 fn main() {
     nannou::app(model).view(view).update(update).run();
@@ -49,21 +85,9 @@ fn model(app: &App) -> Model {
     // The gpu device associated with the window's swapchain
     let device = app.main_window().swapchain().device().clone();
 
-    let positions = [
-        [-1.0, -1.0],
-        [-1.0, 1.0],
-        [1.0, -1.0],
-        [1.0, 1.0],
-    ];
-    let data = positions
-        .iter()
-        .map(|&position| Vertex {
-            position,
-        });
-    let queue = app
-        .main_window()
-        .swapchain_queue()
-        .clone();
+    let positions = [[-1.0, -1.0], [-1.0, 1.0], [1.0, -1.0], [1.0, 1.0]];
+    let data = positions.iter().map(|&position| Vertex { position });
+    let queue = app.main_window().swapchain_queue().clone();
     let usage = vk::BufferUsage::all();
     let (vertex_buffer, buffer_future) =
         vk::ImmutableBuffer::from_iter(data, usage, queue).unwrap();
@@ -74,7 +98,7 @@ fn model(app: &App) -> Model {
         .wait(None)
         .expect("failed to wait for buffer and image creation future");
     let buffer_pool = vk::CpuBufferPool::uniform_buffer(device.clone());
-
+    let audio_buffer_pool = vk::CpuBufferPool::upload(device.clone());
 
     // Get the paths to your vertex and fragment shaders.
     let vert_path = concat!(
@@ -152,6 +176,20 @@ fn model(app: &App) -> Model {
     // can draw we also need to create the actual framebuffer.
     let view_fbo = RefCell::new(ViewFbo::default());
 
+    let shader_info = ShaderInfo { time_delta: 0.0 };
+
+    let channel_data = load_textures(app, device.clone());
+
+    let (tx, audio_data) = mpsc::channel();
+    let buf = [0u8; 64];
+    let audio_stream = app
+        .audio
+        .new_input_stream(Audio { tx, buf }, audio)
+        .build()
+        .expect("Failed to create audio stream");
+    audio_stream.play();
+    let audio_buffer = vec![[0u8; 4]; 512 * 512];
+    let audio_index = (0..(512 * 512)).cycle();
     let mut model = Model {
         render_pass,
         pipeline,
@@ -164,7 +202,15 @@ fn model(app: &App) -> Model {
         device,
         pool,
         buffer_pool,
+        audio_buffer_pool,
+        shader_info,
+        channel_data,
+        _audio_stream: audio_stream,
+        audio_data,
+        audio_buffer,
+        audio_index,
     };
+
     // Here we won't to update the pipeline but
     // we need data from the model so it has to
     // happen after model is created.
@@ -174,7 +220,24 @@ fn model(app: &App) -> Model {
     model
 }
 
-fn update(_app: &App, model: &mut Model, _: Update) {
+fn audio(audio: &mut Audio, s: &Buffer<u16>) {
+    for (i, j) in s.iter().step_by(2).map(|&b| b as u8).zip(audio.buf.iter_mut()) {
+        *j = i;
+    }
+    audio.tx.send(audio.buf).ok();
+}
+
+fn update(_app: &App, model: &mut Model, update: Update) {
+    for buffer in model.audio_data.try_iter().take(4096) {
+        model.audio_index.next();
+        for d in buffer.chunks_exact(4) {
+            let n = model.audio_index.next().unwrap();
+            model.audio_buffer[n].copy_from_slice(d);
+        }
+    }
+
+    model.shader_info.time_delta = (update.since_last.as_secs() as f64
+        + update.since_last.subsec_nanos() as f64 * 1e-9) as f32;
     // Get the latest message from the watcher.
     // There will be a message for any change to the shaders.
     let shader_msg = model.shade_watcher.rx.try_iter().last();
@@ -220,19 +283,54 @@ fn view(app: &App, model: &Model, frame: Frame) -> Frame {
 
     let push_constants = PushConstantData {
         time: app.time,
+        time_delta: model.shader_info.time_delta,
+        frame: app.elapsed_frames() as u32,
+        frame_rate: (60.0 / model.shader_info.time_delta) as u32,
     };
 
     let uniform_buffer = UniformData {
         resolution: [w as _, h as _, 1.0],
     };
 
-    let sub_buffer = model.buffer_pool.next(uniform_buffer).expect("Failed to get next uniform buffer");
+    let sub_buffer = model
+        .buffer_pool
+        .next(uniform_buffer)
+        .expect("Failed to get next uniform buffer");
+    let audio_sub_buffer = model
+        .audio_buffer_pool
+        .chunk(model.audio_buffer.iter().cloned())
+        .expect("Failed to get next uniform buffer");
 
-
-    let descriptor_set = model.pool.as_ref().unwrap().borrow_mut().next()
+    let descriptor_set = model
+        .pool
+        .as_ref()
+        .unwrap()
+        .borrow_mut()
+        .next()
         .add_buffer(sub_buffer)
         .expect("Failed to add uniform buffer")
-        .build().expect("Failed to get next descriptor set");
+        .add_sampled_image(
+            model.channel_data.static_channels[0].clone(),
+            model.channel_data.samplers[0].clone(),
+        )
+        .expect("Failed to add texture sampler")
+        .add_sampled_image(
+            model.channel_data.static_channels[1].clone(),
+            model.channel_data.samplers[1].clone(),
+        )
+        .expect("Failed to add texture sampler")
+        .add_sampled_image(
+            model.channel_data.static_channels[2].clone(),
+            model.channel_data.samplers[2].clone(),
+        )
+        .expect("Failed to add texture sampler")
+        .add_sampled_image(
+            model.channel_data.dynamic_channels[0].clone(),
+            model.channel_data.samplers[3].clone(),
+        )
+        .expect("Failed to add texture sampler")
+        .build()
+        .expect("Failed to get next descriptor set");
 
     // Update the view_fbo.
     model
@@ -249,6 +347,11 @@ fn view(app: &App, model: &Model, frame: Frame) -> Frame {
     // Submit the draw commands.
     frame
         .add_commands()
+        .copy_buffer_to_image(
+            audio_sub_buffer,
+            model.channel_data.dynamic_channels[0].clone(),
+        )
+        .unwrap()
         .begin_render_pass(model.view_fbo.borrow().expect_inner(), false, clear_values)
         .unwrap()
         .draw(
@@ -323,4 +426,92 @@ fn update_pipeline(model: &mut Model) {
             .build(device.clone())
             .unwrap(),
     ));
+}
+
+fn load_textures(app: &App, device: Arc<vk::Device>) -> ChannelData {
+    let dims = [512, 512];
+    let (texture, tex_future) = {
+        let mut texture_path = app.assets_path().expect("Failed to load asset path");
+        texture_path.push(PathBuf::from("images/noise/blue/512_512/LDR_RGBA_0.png"));
+        let image = image::open(texture_path).unwrap().to_rgba();
+        let (width, height) = image.dimensions();
+        let image_data = image.into_raw().clone();
+
+        vk::ImmutableImage::from_iter(
+            image_data.iter().cloned(),
+            vk::image::Dimensions::Dim2d { width, height },
+            vk::Format::R8G8B8A8Srgb,
+            app.main_window().swapchain_queue().clone(),
+        )
+        .unwrap()
+    };
+    let (green_texture, empty_tex_future_1) = vk::ImmutableBuffer::from_iter(
+        (0..(512 * 512)).map(|_| [0u8, 255u8, 0u8, 0u8]),
+        vk::BufferUsage::transfer_source(),
+        app.main_window().swapchain_queue().clone(),
+    )
+    .unwrap();
+
+    let dynamic_image = vk::AttachmentImage::with_usage(
+        device.clone(),
+        dims,
+        vk::Format::R8G8B8A8Srgb,
+        vk::ImageUsage {
+            transfer_destination: true,
+            sampled: true,
+            ..vk::ImageUsage::none()
+        },
+    )
+    .expect("Failed to create attachment image");
+    let samplers = (0..4)
+        .map(|_| {
+            vk::SamplerBuilder::new()
+                .address_u(vk::SamplerAddressMode::Repeat)
+                .address_v(vk::SamplerAddressMode::Repeat)
+                .build(device.clone())
+                .expect("Failed to create sampler")
+        })
+        .collect::<Vec<_>>();
+    tex_future
+        .join(empty_tex_future_1)
+        .then_signal_fence_and_flush()
+        .expect("`then_signal_fence_and_flush` failed")
+        .wait(None)
+        .expect("failed to wait for futures");
+
+    let (green_image_1, green_tex_future_1) = vk::ImmutableImage::from_buffer(
+        green_texture.clone(),
+        vk::image::Dimensions::Dim2d {
+            width: dims[0],
+            height: dims[1],
+        },
+        vk::Format::R8G8B8A8Srgb,
+        app.main_window().swapchain_queue().clone(),
+    )
+    .expect("Failed to create empty image");
+    let (green_image_2, green_tex_future_2) = vk::ImmutableImage::from_buffer(
+        green_texture,
+        vk::image::Dimensions::Dim2d {
+            width: dims[0],
+            height: dims[1],
+        },
+        vk::Format::R8G8B8A8Srgb,
+        app.main_window().swapchain_queue().clone(),
+    )
+    .expect("Failed to create empty image");
+
+    green_tex_future_1
+        .join(green_tex_future_2)
+        .then_signal_fence_and_flush()
+        .expect("`then_signal_fence_and_flush` failed")
+        .wait(None)
+        .expect("failed to wait for futures");
+
+    let static_channels = vec![texture, green_image_1, green_image_2];
+    let dynamic_channels = vec![dynamic_image];
+    ChannelData {
+        static_channels,
+        dynamic_channels,
+        samplers,
+    }
 }
