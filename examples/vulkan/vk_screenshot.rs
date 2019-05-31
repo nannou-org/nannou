c4daafba5fc1a8de6a15b575a452feaedba90845
Tom Gowan <s3549186@student.rmit.edu.au>
2019-05-31 10:43:03 +1000

    needs blit

--- examples/vulkan/vk_screenshot.rs
+++ examples/vulkan/vk_screenshot.rs
@@ -1,17 +1,32 @@
+use nannou::color;
 use nannou::prelude::*;
-use std::cell::RefCell;
+use palette::named;
+use std::cell::{Cell, RefCell};
+use std::convert::TryInto;
 use std::sync::Arc;
+use std::slice;
+
+// These must be smaller then your actual screen
+const IMAGE_DIMS: (usize, usize) = (1366, 600);
+// This must match the number of colours per
+// pixel.
+// RGBA = 4
+// RGB = 3
+// RG = 2 etc.
+const NUM_COLOURS: usize = 4;
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).update(update).run();
 }
 
 struct Model {
     render_pass: Arc<vk::RenderPassAbstract + Send + Sync>,
     pipeline: Arc<vk::GraphicsPipelineAbstract + Send + Sync>,
-    //vertex_buffer: Arc<vk::CpuAccessibleBuffer<[Vertex]>>,
-    vertex_buffer_pool: vk::CpuBufferPool<[Vertex]>,
+    vertex_buffer_pool: vk::CpuBufferPool<[Vertex; 3]>,
     view_fbo: RefCell<ViewFbo>,
+    screenshot_buffer: Arc<vk::CpuAccessibleBuffer<[[u8; NUM_COLOURS]]>>,
+    take_screenshot: Cell<bool>,
+    save_screenshot: Cell<bool>,
 }
 
 #[derive(Debug, Clone)]
@@ -23,24 +38,27 @@ vk::impl_vertex!(Vertex, position);
 
 fn model(app: &App) -> Model {
     app.new_window()
-        .with_dimensions(512, 512)
+        .with_dimensions(
+            IMAGE_DIMS.0.try_into().unwrap(),
+            IMAGE_DIMS.1.try_into().unwrap(),
+        )
         .view(view)
+        .event(window_event)
         .build()
         .unwrap();
 
     // The gpu device associated with the window's swapchain
     let device = app.main_window().swapchain().device().clone();
+    dbg!(app.main_window().swapchain().format());
 
+    let buf = vec![[0u8; NUM_COLOURS]; IMAGE_DIMS.0 * IMAGE_DIMS.1];
     let vertex_buffer_pool = vk::CpuBufferPool::vertex_buffer(device.clone());
-    // We now create a buffer that will store the shape of our triangle.
-    /*
-    let vertex_buffer = {
-        let positions = [[-0.5, -0.25], [0.0, 0.5], [0.25, -0.1]];
-        let vertices = positions.iter().map(|&position| Vertex { position });
-        vk::CpuAccessibleBuffer::from_iter(device.clone(), vk::BufferUsage::all(), vertices)
-            .unwrap()
-    };
-    */
+    let screenshot_buffer = vk::CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        vk::BufferUsage::transfer_destination(),
+        buf.into_iter(),
+    )
+    .expect("Failed to create screenshot buffer");
 
     let vertex_shader = vs::Shader::load(device.clone()).unwrap();
     let fragment_shader = fs::Shader::load(device.clone()).unwrap();
@@ -114,11 +132,52 @@ fn model(app: &App) -> Model {
         pipeline,
         vertex_buffer_pool,
         view_fbo,
+        screenshot_buffer,
+        take_screenshot: Cell::new(false),
+        save_screenshot: Cell::new(false),
+    }
+}
+
+fn write(screenshot_buffer: &[[u8; NUM_COLOURS]]) {
+    // This is pretty unsafe but
+    // there is no fast and safe way to go from
+    // [[u8; NUM_COLOURS]; IMAGE_DIMS.0 * IMAGE_DIMS.1]
+    // to
+    // [u8; NUM_COLOURS * IMAGE_DIMS.0 * IMAGE_DIMS.1]
+    let buf: &[u8] = unsafe {
+        slice::from_raw_parts(&screenshot_buffer[0] as *const u8, NUM_COLOURS * IMAGE_DIMS.0 * IMAGE_DIMS.1)
+    };
+
+    let screenshot_path = concat!(env!("CARGO_MANIFEST_DIR"), "/screenshot.png");
+    // It is vital that ColorType(bit_depth) matches the
+    // type that is used in the screenshot buffer
+    nannou::image::save_buffer(
+        screenshot_path,
+        buf,
+        IMAGE_DIMS.0 as u32,
+        IMAGE_DIMS.1 as u32,
+        nannou::image::ColorType::BGRA(8),
+        )
+        .expect("Failed to save image");
+}
+
+fn update(_app: &App, model: &mut Model, _: Update) {
+    let ss = model.save_screenshot.replace(false);
+    if ss {
+        let screenshot_buffer = match model.screenshot_buffer.read() {
+            Ok(s) => s,
+            Err(_) => {
+                model.save_screenshot.set(true);
+                return ();
+            },
+        };
+        write(&(*screenshot_buffer));
+        model.take_screenshot.set(true);
     }
 }
 
 // Draw the state of your `Model` into the given `Frame` here.
-fn view(_app: &App, model: &Model, frame: Frame) -> Frame {
+fn view(app: &App, model: &Model, frame: Frame) -> Frame {
     // Dynamic viewports allow us to recreate just the viewport when the window is resized
     // Otherwise we would have to recreate the whole pipeline.
     let [w, h] = frame.swapchain_image().dimensions();
@@ -134,32 +193,112 @@ fn view(_app: &App, model: &Model, frame: Frame) -> Frame {
         })
         .unwrap();
 
-    let positions = [[-0.5, -0.25], [0.0, 0.5], [0.25, -0.1]];
-    let vertices = positions.iter().map(|&position| Vertex { position });
+    let n = (1.0 + app.time.sin()) / 2.0;
+
+    let vertices = [
+        Vertex {
+            position: [-0.5 * n, -0.25],
+        },
+        Vertex {
+            position: [0.0, 0.5 * n],
+        },
+        Vertex {
+            position: [0.25 * n, -0.1],
+        },
+    ];
+
     let vertex_buffer = model
-        .buffer_pool
+        .vertex_buffer_pool
         .next(vertices)
         .expect("Failed to get next uniform buffer");
+
+    let vertex_buffer = Arc::new(vertex_buffer);
+
     // Specify the color to clear the framebuffer with i.e. blue.
-    let clear_values = vec![[0.0, 0.0, 1.0, 1.0].into()];
+    let b = color::Alpha {
+        color: Rgb::from_pixel(&named::ALICEBLUE),
+        alpha: 1.0,
+    };
+    let b: [f32; 4] = b.to_pixel();
+    let clear_values = vec![b.into()];
 
     // Submit the draw commands.
-    frame
-        .add_commands()
-        .begin_render_pass(model.view_fbo.borrow().expect_inner(), false, clear_values)
-        .unwrap()
-        .draw(
-            model.pipeline.clone(),
-            &dynamic_state,
-            vec![model.vertex_buffer.clone()],
-            (),
-            (),
-        )
-        .unwrap()
-        .end_render_pass()
-        .expect("failed to add `end_render_pass` command");
+    let ts = model.take_screenshot.replace(false);
+    if ts {
+        model.save_screenshot.set(true);
+        frame
+            .add_commands()
+            .begin_render_pass(model.view_fbo.borrow().expect_inner(), false, clear_values)
+            .unwrap()
+            .draw(
+                model.pipeline.clone(),
+                &dynamic_state,
+                vec![vertex_buffer],
+                (),
+                (),
+            )
+            .unwrap()
+            .end_render_pass()
+            .expect("failed to add `end_render_pass` command")
+            .copy_image_to_buffer_dimensions(
+                frame.swapchain_image().clone(),
+                model.screenshot_buffer.clone(),
+                [0, 0, 0],
+                [IMAGE_DIMS.0 as u32, IMAGE_DIMS.1 as u32, 1],
+                0,
+                1,
+                0,
+            )
+            .expect("failed to copy image");
 
-    frame
+        frame
+    } else {
+        frame
+            .add_commands()
+            .begin_render_pass(model.view_fbo.borrow().expect_inner(), false, clear_values)
+            .unwrap()
+            .draw(
+                model.pipeline.clone(),
+                &dynamic_state,
+                vec![vertex_buffer],
+                (),
+                (),
+            )
+            .unwrap()
+            .end_render_pass()
+            .expect("failed to add `end_render_pass` command");
+
+        frame
+    }
+}
+
+fn window_event(_app: &App, model: &mut Model, event: WindowEvent) {
+    match event {
+        KeyPressed(key) => {
+            if let Key::S = key {
+                if !model.save_screenshot.get() {
+                    model.take_screenshot.set(true);
+                }
+            }
+        }
+        KeyReleased(_key) => {}
+        MouseMoved(_pos) => {}
+        MousePressed(_button) => {}
+        MouseReleased(_button) => {}
+        MouseEntered => {}
+        MouseExited => {}
+        MouseWheel(_amount, _phase) => {}
+        Moved(_pos) => {}
+        Resized(_size) => {}
+        Touch(_touch) => {}
+        TouchPressure(_pressure) => {}
+        HoveredFile(_path) => {}
+        DroppedFile(_path) => {}
+        HoveredFileCancelled => {}
+        Focused => {}
+        Unfocused => {}
+        Closed => {}
+    }
 }
 
 mod vs {
