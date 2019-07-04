3a243fa4fe97e5c66e09034208288d7550a07892
mitchmindtree <mitchell.nordine@gmail.com>
2019-07-04 21:18:57 +0200

    Add missing inherent methods from Device impl
    This adds an inherent method for each of the `cpal::Device` trait
    methods, clarifying documentation and removing the need to manually
    import the Device trait to use the nannou_audio `Device` type.

--- src/device.rs
+++ src/device.rs
@@ -12,7 +12,46 @@ pub struct Devices {
     pub(crate) devices: CpalDevices,
 }
 
+/// An iterator yielding formats that are supported by the backend.
+pub type SupportedInputFormats = cpal::platform::SupportedInputFormats;
+
+/// An iterator yielding formats that are supported by the backend.
+pub type SupportedOutputFormats = cpal::platform::SupportedOutputFormats;
+
 impl Device {
+    /// The unique name associated with this device.
+    pub fn name(&self) -> Result<String, cpal::DeviceNameError> {
+        self.device.name()
+    }
+
+    /// An iterator yielding formats that are supported by the backend.
+    ///
+    /// Can return an error if the device is no longer valid (e.g. it has been disconnected).
+    pub fn supported_input_formats(
+        &self
+    ) -> Result<SupportedInputFormats, cpal::SupportedFormatsError> {
+        self.device.supported_input_formats()
+    }
+
+    /// An iterator yielding formats that are supported by the backend.
+    ///
+    /// Can return an error if the device is no longer valid (e.g. it has been disconnected).
+    pub fn supported_output_formats(
+        &self
+    ) -> Result<SupportedOutputFormats, cpal::SupportedFormatsError> {
+        self.device.supported_output_formats()
+    }
+
+    /// The default format used for input streams.
+    pub fn default_input_format(&self) -> Result<cpal::Format, cpal::DefaultFormatError> {
+        self.device.default_input_format()
+    }
+
+    /// The default format used for output streams.
+    pub fn default_output_format(&self) -> Result<cpal::Format, cpal::DefaultFormatError> {
+        self.device.default_output_format()
+    }
+
     /// The maximum number of output channels of any format supported by this device.
     pub fn max_supported_output_channels(&self) -> usize {
         self.supported_output_formats()
