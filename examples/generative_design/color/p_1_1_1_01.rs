02de6053438ee01ec487992a3d34e6919bc5f5b1
JoshuaBatty <joshpbatty@gmail.com>
2019-08-26 18:02:59 +0200

    adding in more generative examples

--- examples/generative_design/color/p_1_1_1_01.rs
+++ examples/generative_design/color/p_1_1_1_01.rs
@@ -1,3 +1,28 @@
+// P_1_1_1_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * draw the color spectrum by moving the mouse
+ *
+ * MOUSE
+ * position x/y        : resolution
+ */
 use nannou::prelude::*;
 
 fn main() {
--- examples/generative_design/color/p_1_2_3_01.rs
+++ examples/generative_design/color/p_1_2_3_01.rs
@@ -1,14 +1,33 @@
+// P_1_2_3_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
-* generates specific color palettes
-*
-* MOUSE
-* position x/y        : row and coloum count
-*
-* KEYS
-* 0-9                 : creates specific color palettes
-* s                   : save png
-* c                   : save color palette
-*/
+ * generates specific color palettes
+ *
+ * MOUSE
+ * position x/y        : row and coloum count
+ *
+ * KEYS
+ * 0-9                 : creates specific color palettes
+ * s                   : save png
+ * c                   : save color palette
+ */
 use nannou::prelude::*;
 
 fn main() {
