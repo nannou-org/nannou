a823c713a4c01e6058724d858bd80a071b08aeaa
mitchmindtree <mitchell.nordine@gmail.com>
2019-08-31 01:26:13 +0200

    Update draw text primitive builder to use new text layout builder type

--- src/draw/primitive/text.rs
+++ src/draw/primitive/text.rs
@@ -7,7 +7,7 @@ use crate::draw::properties::{
 use crate::draw::{theme, Drawing};
 use crate::geom;
 use crate::math::BaseFloat;
-use crate::text;
+use crate::text::{self, Align, Font, FontSize, Justify, Layout, Scalar, Wrap};
 
 /// Properties related to drawing the **Text** primitive.
 #[derive(Clone, Debug)]
@@ -22,28 +22,11 @@ pub struct Text<S = geom::scalar::Default> {
 #[derive(Clone, Debug, Default)]
 pub struct Style {
     pub color: Option<LinSrgba>,
-    pub line_spacing: Option<text::Scalar>,
-    pub maybe_wrap: Option<Option<Wrap>>,
-    pub font_size: Option<text::FontSize>,
-    pub justify: Option<text::Justify>,
-    pub font_id: Option<Option<text::font::Id>>,
-}
-
-/// The way in which text should wrap around the width.
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Wrap {
-    /// Wrap at the first character that exceeds the width.
-    Character,
-    /// Wrap at the first word that exceeds the width.
-    Whitespace,
+    pub layout: text::layout::Builder,
 }
 
 /// The drawing context for the **Text** primitive.
 pub type DrawingText<'a, S = geom::scalar::Default> = Drawing<'a, Text<S>, S>;
-
-pub const DEFAULT_WRAP: Option<Wrap> = Some(Wrap::Whitespace);
-pub const DEFAULT_FONT_SIZE: u32 = 12;
-pub const DEFAULT_LINE_SPACING: f32 = 1.0;
 
 impl<S> Text<S> {
     /// Begin drawing some text.
@@ -61,66 +44,108 @@ impl<S> Text<S> {
         }
     }
 
-    /// The font size to use for the text.
-    pub fn font_size(mut self, size: text::FontSize) -> Self {
-        self.style.font_size = Some(size);
+    // Apply the given function to the inner text layout.
+    fn map_layout<F>(mut self, map: F) -> Self
+    where
+        F: FnOnce(text::layout::Builder) -> text::layout::Builder,
+    {
+        self.style.layout = map(self.style.layout);
         self
+    }
+
+    /// The font size to use for the text.
+    pub fn font_size(self, size: FontSize) -> Self {
+        self.map_layout(|l| l.font_size(size))
+    }
+
+    /// Specify whether or not text should be wrapped around some width and how to do so.
+    ///
+    /// The default value is `DEFAULT_LINE_WRAP`.
+    pub fn line_wrap(self, line_wrap: Option<Wrap>) -> Self {
+        self.map_layout(|l| l.line_wrap(line_wrap))
     }
 
     /// Specify that the **Text** should not wrap lines around the width.
-    pub fn no_line_wrap(mut self) -> Self {
-        self.style.maybe_wrap = Some(None);
-        self
+    ///
+    /// Shorthand for `builder.line_wrap(None)`.
+    pub fn no_line_wrap(self) -> Self {
+        self.map_layout(|l| l.no_line_wrap())
     }
 
     /// Line wrap the **Text** at the beginning of the first word that exceeds the width.
-    pub fn wrap_by_word(mut self) -> Self {
-        self.style.maybe_wrap = Some(Some(Wrap::Whitespace));
-        self
+    ///
+    /// Shorthand for `builder.line_wrap(Some(Wrap::Whitespace))`.
+    pub fn wrap_by_word(self) -> Self {
+        self.map_layout(|l| l.wrap_by_word())
     }
 
     /// Line wrap the **Text** at the beginning of the first character that exceeds the width.
-    pub fn wrap_by_character(mut self) -> Self {
-        self.style.maybe_wrap = Some(Some(Wrap::Character));
-        self
+    ///
+    /// Shorthand for `builder.line_wrap(Some(Wrap::Character))`.
+    pub fn wrap_by_character(self) -> Self {
+        self.map_layout(|l| l.wrap_by_character())
     }
 
     /// A method for specifying the `Font` used for displaying the `Text`.
-    pub fn font_id(mut self, font_id: text::font::Id) -> Self {
-        self.style.font_id = Some(Some(font_id));
-        self
-    }
-
-    /// Build the **Text** with the given **Style**.
-    pub fn with_style(mut self, style: Style) -> Self {
-        self.style = style;
-        self
+    pub fn font(self, font: Font) -> Self {
+        self.map_layout(|l| l.font(font))
     }
 
     /// Describe the end along the *x* axis to which the text should be aligned.
-    pub fn justify(mut self, justify: text::Justify) -> Self {
-        self.style.justify = Some(justify);
-        self
+    pub fn justify(self, justify: Justify) -> Self {
+        self.map_layout(|l| l.justify(justify))
     }
 
     /// Align the text to the left of its bounding **Rect**'s *x* axis range.
     pub fn left_justify(self) -> Self {
-        self.justify(text::Justify::Left)
+        self.map_layout(|l| l.left_justify())
     }
 
     /// Align the text to the middle of its bounding **Rect**'s *x* axis range.
     pub fn center_justify(self) -> Self {
-        self.justify(text::Justify::Center)
+        self.map_layout(|l| l.center_justify())
     }
 
     /// Align the text to the right of its bounding **Rect**'s *x* axis range.
     pub fn right_justify(self) -> Self {
-        self.justify(text::Justify::Right)
+        self.map_layout(|l| l.right_justify())
     }
 
     /// Specify how much vertical space should separate each line of text.
-    pub fn line_spacing(mut self, spacing: text::Scalar) -> Self {
-        self.style.line_spacing = Some(spacing);
+    pub fn line_spacing(self, spacing: Scalar) -> Self {
+        self.map_layout(|l| l.line_spacing(spacing))
+    }
+
+    /// Specify how the whole text should be aligned along the y axis of its bounding rectangle
+    pub fn y_align(self, align: Align) -> Self {
+        self.map_layout(|l| l.y_align(align))
+    }
+
+    /// Align the top edge of the text with the top edge of its bounding rectangle.
+    pub fn align_top(self) -> Self {
+        self.map_layout(|l| l.align_top())
+    }
+
+    /// Align the middle of the text with the middle of the bounding rect along the y axis..
+    ///
+    /// This is the default behaviour.
+    pub fn align_middle_y(self) -> Self {
+        self.map_layout(|l| l.align_middle_y())
+    }
+
+    /// Align the bottom edge of the text with the bottom edge of its bounding rectangle.
+    pub fn align_bottom(self) -> Self {
+        self.map_layout(|l| l.align_bottom())
+    }
+
+    /// Set all the parameters via an existing `Layout`
+    pub fn layout(self, layout: &Layout) -> Self {
+        self.map_layout(|l| l.layout(layout))
+    }
+
+    /// Specify the entire styling for the **Text**.
+    pub fn with_style(mut self, style: Style) -> Self {
+        self.style = style;
         self
     }
 }
@@ -150,8 +175,8 @@ where
     }
 
     /// A method for specifying the `Font` used for displaying the `Text`.
-    pub fn font_id(self, font_id: text::font::Id) -> Self {
-        self.map_ty(|ty| ty.font_id(font_id))
+    pub fn font(self, font: text::Font) -> Self {
+        self.map_ty(|ty| ty.font(font))
     }
 
     /// Build the **Text** with the given **Style**.
@@ -182,6 +207,33 @@ where
     /// Specify how much vertical space should separate each line of text.
     pub fn line_spacing(self, spacing: text::Scalar) -> Self {
         self.map_ty(|ty| ty.line_spacing(spacing))
+    }
+
+    /// Specify how the whole text should be aligned along the y axis of its bounding rectangle
+    pub fn y_align_text(self, align: Align) -> Self {
+        self.map_ty(|ty| ty.y_align(align))
+    }
+
+    /// Align the top edge of the text with the top edge of its bounding rectangle.
+    pub fn align_text_top(self) -> Self {
+        self.map_ty(|ty| ty.align_top())
+    }
+
+    /// Align the middle of the text with the middle of the bounding rect along the y axis..
+    ///
+    /// This is the default behaviour.
+    pub fn align_text_middle_y(self) -> Self {
+        self.map_ty(|ty| ty.align_middle_y())
+    }
+
+    /// Align the bottom edge of the text with the bottom edge of its bounding rectangle.
+    pub fn align_text_bottom(self) -> Self {
+        self.map_ty(|ty| ty.align_bottom())
+    }
+
+    /// Set all the parameters via an existing `Layout`
+    pub fn layout(self, layout: &Layout) -> Self {
+        self.map_ty(|ty| ty.layout(layout))
     }
 }
 
