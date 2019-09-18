af7a16f05747f937cccac95e315a6d6875b15515
JoshuaBatty <joshpbatty@gmail.com>
2019-09-18 18:58:30 +0200

    adds in 4 more noc systems examples

---
+++ examples/nature_of_code/chp_04_systems/4_01_single_particle_trail.rs
@@ -0,0 +1,115 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+//
+// example 4-01: Single Particle
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+// A simple particle type
+struct Particle {
+    position: Point2,
+    velocity: Vector2,
+    acceleration: Vector2,
+    life_span: f32,
+}
+
+impl Particle {
+    fn new(l: Point2) -> Self {
+        let acceleration = vec2(0.0, 0.05);
+        let velocity = vec2(random_f32() * 2.0 - 1.0, random_f32() - 1.0);
+        let position = l;
+        let life_span = 255.0;
+        Particle {
+            acceleration,
+            velocity,
+            position,
+            life_span,
+        }
+    }
+
+    // Method to update position
+    fn update(&mut self) {
+        self.velocity += self.acceleration;
+        self.position -= self.velocity;
+        self.life_span -= 2.0;
+    }
+
+    // Method to display
+    fn display(&self, draw: &app::Draw) {
+        draw.ellipse()
+            .xy(self.position)
+            .w_h(12.0, 12.0)
+            .rgba(0.5, 0.5, 0.5, self.life_span / 255.0)
+            .stroke(rgba(0.0, 0.0, 0.0, self.life_span / 255.0))
+            .stroke_weight(2.0);
+    }
+
+    // Is the poarticel still useful?
+    fn is_dead(&self) -> bool {
+        if self.life_span < 0.0 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+struct Model {
+    p: Particle,
+    mouse_down: bool,
+}
+
+fn model(app: &App) -> Model {
+    app.new_window()
+        .with_dimensions(800, 200)
+        .view(view)
+        .mouse_pressed(mouse_pressed)
+        .mouse_released(mouse_released)
+        .build()
+        .unwrap();
+    let p = Particle::new(pt2(0.0, app.window_rect().top() - 20.0));
+    Model {
+        p,
+        mouse_down: false,
+    }
+}
+
+fn update(_app: &App, m: &mut Model, _update: Update) {
+    if m.mouse_down {
+        m.p.update();
+        if m.p.is_dead() {
+            println!("Particle dead!");
+        }
+    }
+}
+
+fn view(app: &App, m: &Model, frame: &Frame) {
+    // Begin drawing
+    let draw = app.draw();
+
+    if app.elapsed_frames() == 1 {
+        draw.background().color(WHITE);
+    }
+
+    if m.mouse_down {
+        draw.rect()
+            .wh(app.window_rect().wh())
+            .rgba(1.0, 1.0, 1.0, 0.03);
+        m.p.display(&draw);
+    }
+
+    // Write the result of our drawing to the window's frame.
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn mouse_pressed(_app: &App, m: &mut Model, _button: MouseButton) {
+    m.mouse_down = true;
+}
+
+fn mouse_released(_app: &App, m: &mut Model, _button: MouseButton) {
+    m.mouse_down = false;
+}
---
+++ examples/nature_of_code/chp_04_systems/4_06_particle_system_forces.rs
@@ -0,0 +1,140 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+//
+// example 4-06: Particle System Forces
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+// A simple particle type
+struct Particle {
+    position: Point2,
+    velocity: Vector2,
+    acceleration: Vector2,
+    life_span: f32,
+    mass: f32,
+}
+
+impl Particle {
+    fn new(l: Point2) -> Self {
+        let acceleration = vec2(0.0, 0.0);
+        let velocity = vec2(random_f32() * 2.0 - 1.0, random_f32() * 2.0 - 2.0);
+        let position = l;
+        let life_span = 255.0;
+        let mass = 1.0; // Let's do something better here!
+
+        Particle {
+            acceleration,
+            velocity,
+            position,
+            life_span,
+            mass,
+        }
+    }
+
+    fn apply_force(&mut self, f: Vector2) {
+        self.acceleration += f / self.mass;
+    }
+
+    // Method to update position
+    fn update(&mut self) {
+        self.velocity += self.acceleration;
+        self.position -= self.velocity;
+        self.acceleration *= 0.0;
+        self.life_span -= 2.0;
+    }
+
+    // Method to display
+    fn display(&self, draw: &app::Draw) {
+        draw.ellipse()
+            .xy(self.position)
+            .radius(6.0)
+            .rgba(0.5, 0.5, 0.5, self.life_span / 255.0)
+            .stroke(rgba(0.0, 0.0, 0.0, self.life_span / 255.0))
+            .stroke_weight(2.0);
+    }
+
+    // Is the particle still useful?
+    fn is_dead(&self) -> bool {
+        if self.life_span < 0.0 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+struct ParticleSystem {
+    particles: Vec<Particle>,
+    pub origin: Point2,
+}
+
+impl ParticleSystem {
+    fn new(position: Point2) -> Self {
+        let origin = position;
+        let particles = Vec::new();
+        ParticleSystem { origin, particles }
+    }
+
+    fn add_particle(&mut self) {
+        self.particles.push(Particle::new(self.origin));
+    }
+
+    // A function to apply a force to all Particles
+    fn apply_force(&mut self, f: Vector2) {
+        for p in self.particles.iter_mut() {
+            p.apply_force(f);
+        }
+    }
+
+    fn update(&mut self) {
+        for i in (0..self.particles.len()).rev() {
+            self.particles[i].update();
+            if self.particles[i].is_dead() {
+                self.particles.remove(i);
+            }
+        }
+    }
+
+    fn draw(&self, draw: &app::Draw) {
+        for p in self.particles.iter() {
+            p.display(&draw);
+        }
+    }
+}
+
+struct Model {
+    ps: ParticleSystem,
+}
+
+fn model(app: &App) -> Model {
+    app.new_window()
+        .with_dimensions(640, 360)
+        .view(view)
+        .build()
+        .unwrap();
+    let ps = ParticleSystem::new(pt2(0.0, app.window_rect().top() - 50.0));
+    Model { ps }
+}
+
+fn update(_app: &App, m: &mut Model, _update: Update) {
+    let gravity = pt2(0.0, 0.1);
+    m.ps.apply_force(gravity);
+
+    m.ps.add_particle();
+    m.ps.update();
+}
+
+fn view(app: &App, m: &Model, frame: &Frame) {
+    // Begin drawing
+    let draw = app.draw();
+    draw.background().color(WHITE);
+
+    m.ps.draw(&draw);
+
+    // Write the result of our drawing to the window's frame.
+    draw.to_frame(app, &frame).unwrap();
+}
