5c73aa16ff136caa3170d62c2df4676af09504d8
mitchmindtree <mitchell.nordine@gmail.com>
2020-03-29 19:44:10 +0200

    Avoid panicking if saved timeline data does not yet exist

--- examples/demo.rs
+++ examples/demo.rs
@@ -420,7 +420,9 @@ fn key_pressed(_app: &App, model: &mut Model, key: Key) {
             // Load the model.timeline_data from a JSON file.
             // This part is only included if you compile with the serde feature enabled.
             #[cfg(feature = "serde1")] {
-                model.timeline_data = nannou::io::load_from_json("./saved_timeline_data.json").expect("Error loading timeline data");
+                if let Ok(new_data) = nannou::io::load_from_json("./saved_timeline_data.json") {
+                    model.timeline_data = new_data;
+                }
             }
         }
         _ => {}
