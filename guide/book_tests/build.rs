e37634056d64535e147c5baa9eed5ecc22f15cdb
mitchmindtree <mitchell.nordine@gmail.com>
2020-03-31 22:07:53 +0200

    Fix paths in book_tests build script. Add book_tests to workspace.
    Also adds a guide test to the CI workflow.

--- guide/book_tests/build.rs
+++ guide/book_tests/build.rs
@@ -1,6 +1,6 @@
 use skeptic::*;
-use std::{env, fs, io};
 use std::path::{Path, PathBuf};
+use std::{env, fs, io};
 
 // Check the given path and parent directories for a path with the given name.
 fn check_parents(name: &str, path: &Path) -> io::Result<Option<PathBuf>> {
@@ -19,19 +19,18 @@ fn check_dir(name: &str, path: &Path) -> io::Result<Option<PathBuf>> {
         let entry = entry?;
         let entry_path = entry.path();
         if entry_path.ends_with(name) {
-            return Ok(Some(entry_path))
+            return Ok(Some(entry_path));
         }
     }
     Ok(None)
 }
 
 fn main() {
-    let out_dir = env::var("OUT_DIR")
-        .expect("failed to retrieve OUT_DIR");
-    let cargo_toml_path = check_parents("Cargo.toml", Path::new(&out_dir))
+    let out_dir = env::var("OUT_DIR").expect("failed to retrieve OUT_DIR");
+    let workspace_cargo_toml_path = check_parents("Cargo.toml", Path::new(&out_dir))
         .expect("an error occurred while searching for Cargo.toml")
         .expect("no Cargo.toml found");
-    let book_root_path = cargo_toml_path.parent().unwrap().parent().unwrap();
+    let book_root_path = workspace_cargo_toml_path.parent().unwrap().join("guide");
     let book_src_path = book_root_path.join("src");
     let book_src_path_str = format!("{}", book_src_path.display());
     let mdbook_files = markdown_files_of_directory(&book_src_path_str);
