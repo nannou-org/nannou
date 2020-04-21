29320176c35a6d7f2dc8134e4145d14c29296505
mitchmindtree <mitchell.nordine@gmail.com>
2020-04-21 21:28:00 +0200

    Simplify template examples a little

--- examples/templates/template_app.rs
+++ examples/templates/template_app.rs
@@ -9,48 +9,15 @@ struct Model {
 }
 
 fn model(app: &App) -> Model {
-    let _window = app
-        .new_window()
-        .size(720, 720)
-        .view(view)
-        .event(window_event)
-        .build()
-        .unwrap();
+    let _window = app.new_window().view(view).build().unwrap();
     Model { _window }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn window_event(_app: &App, _model: &mut Model, event: WindowEvent) {
-    match event {
-        KeyPressed(_key) => {}
-        KeyReleased(_key) => {}
-        MouseMoved(_pos) => {}
-        MousePressed(_button) => {}
-        MouseReleased(_button) => {}
-        MouseEntered => {}
-        MouseExited => {}
-        MouseWheel(_amount, _phase) => {}
-        Moved(_pos) => {}
-        Resized(_size) => {}
-        Touch(_touch) => {}
-        TouchPressure(_pressure) => {}
-        HoveredFile(_path) => {}
-        DroppedFile(_path) => {}
-        HoveredFileCancelled => {}
-        Focused => {}
-        Unfocused => {}
-        Closed => {}
-    }
-}
-
 fn view(app: &App, _model: &Model, frame: Frame) {
-    // Prepare to draw.
     let draw = app.draw();
-    // Clear the background to pink.
     draw.background().color(PLUM);
-    // Draw a red ellipse with default size and position.
     draw.ellipse().color(STEELBLUE);
-    // Write to the window frame.
     draw.to_frame(app, &frame).unwrap();
 }
--- examples/templates/template_sketch.rs
+++ examples/templates/template_sketch.rs
@@ -5,15 +5,8 @@ fn main() {
 }
 
 fn view(app: &App, frame: Frame) {
-    // Prepare to draw.
     let draw = app.draw();
-
-    // Clear the background to purple.
     draw.background().color(PLUM);
-
-    // Draw a blue ellipse with default size and position.
     draw.ellipse().color(STEELBLUE);
-
-    // Write to the window frame.
     draw.to_frame(app, &frame).unwrap();
 }
