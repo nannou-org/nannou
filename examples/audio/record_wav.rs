ed2b111bc5ceedd6797955dacdc01db902768f9c
zehreken <zehreken@gmail.com>
2020-08-27 21:36:17 +0200

    Add record indicator and clean-up

--- examples/audio/record_wav.rs
+++ examples/audio/record_wav.rs
@@ -1,4 +1,4 @@
-//! Records a WAV file (roughly 3 seconds long) using the default input device and config.
+//! Records a WAV file using the default input device and default input format.
 //!
 //! The input data is recorded to "$CARGO_MANIFEST_DIR/recorded.wav".
 use nannou::prelude::*;
@@ -33,13 +33,8 @@ fn model(app: &App) -> Model {
     // Initialise the audio host so we can spawn an audio stream.
     let audio_host = audio::Host::new();
 
-    // Get specs from device config.
-    let spec = hound::WavSpec {
-        channels: 1,
-        sample_rate: 44100,
-        bits_per_sample: 32,
-        sample_format: hound::SampleFormat::Float,
-    };
+    // Create a writer
+    let spec = get_spec(&audio_host);
     let writer = hound::WavWriter::create("recorded.wav", spec).unwrap();
     let capture_model = CaptureModel { writer };
 
@@ -55,24 +50,59 @@ fn model(app: &App) -> Model {
 // A function that captures the audio from the buffer and
 // writes it into the the WavWriter.
 fn capture_fn(audio: &mut CaptureModel, buffer: &Buffer) {
-    // when the program ends, writer is dropped and
-    // when writer is dropped, it writes data to disk.
+    // When the program ends, writer is dropped and data gets written to the disk.
     for frame in buffer.frames() {
-        audio
-            .writer
-            .write_sample(frame[0] as f32)
-            .expect("error while writing");
+        for sample in frame {
+            audio
+                .writer
+                .write_sample(*sample)
+                .expect("error while writing the sample");
+        }
     }
 }
 
 fn key_pressed(_app: &App, model: &mut Model, key: Key) {
     match key {
-        Key::R => {}
-        Key::P => {}
+        Key::Space => {
+            if model.stream.is_paused() {
+                model.stream.play().unwrap();
+            } else if model.stream.is_playing() {
+                model.stream.pause().unwrap();
+            }
+        }
         _ => {}
     }
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     frame.clear(DIMGRAY);
+
+    if model.stream.is_playing() && app.elapsed_frames() % 30 < 20 {
+        let draw = app.draw();
+        draw.ellipse().w_h(100.0, 100.0).color(RED);
+
+        draw.to_frame(app, &frame).unwrap();
+    }
+}
+
+// Get specification from default device format.
+fn get_spec(audio_host: &nannou_audio::Host) -> hound::WavSpec {
+    let default_input_format = audio_host
+        .default_input_device()
+        .unwrap()
+        .default_input_format()
+        .unwrap();
+
+    let (bits_per_sample, sample_format) = match default_input_format.data_type {
+        audio::cpal::SampleFormat::I16 => (16, hound::SampleFormat::Int),
+        audio::cpal::SampleFormat::U16 => (16, hound::SampleFormat::Int),
+        audio::cpal::SampleFormat::F32 => (32, hound::SampleFormat::Float),
+    };
+
+    hound::WavSpec {
+        channels: default_input_format.channels,
+        sample_rate: default_input_format.sample_rate.0,
+        bits_per_sample,
+        sample_format,
+    }
 }
