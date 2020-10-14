e64726f560afd0a01713d445eddd6a552e8d4067
zehreken <zehreken@gmail.com>
2020-10-14 21:25:03 +0200

    Refactor and add more comments

--- examples/audio/feedback.rs
+++ examples/audio/feedback.rs
@@ -1,3 +1,6 @@
+//! Feeds back the input stream directly into the output stream
+//!
+//! You can play and pause the streams by pressing space key
 use nannou::prelude::*;
 use nannou_audio as audio;
 use nannou_audio::Buffer;
@@ -8,16 +11,16 @@ fn main() {
 }
 
 struct Model {
-    in_stream: audio::Stream<InModel>,
-    out_stream: audio::Stream<OutModel>,
+    in_stream: audio::Stream<InputModel>,
+    out_stream: audio::Stream<OutputModel>,
 }
 
-struct InModel {
-    pub sender: Producer<[f32; 2]>,
+struct InputModel {
+    pub producer: Producer<[f32; 2]>,
 }
 
-struct OutModel {
-    pub receiver: Consumer<[f32; 2]>,
+struct OutputModel {
+    pub consumer: Consumer<[f32; 2]>,
 }
 
 fn model(app: &App) -> Model {
@@ -31,16 +34,20 @@ fn model(app: &App) -> Model {
     // Initialise the audio host so we can spawn an audio stream.
     let audio_host = audio::Host::new();
 
+    // Create a ring buffer and split it into producer and consumer
     let ring_buffer = RingBuffer::<[f32; 2]>::new(1024 * 2); // Add some latency
-    let (mut prod, cons) = ring_buffer.split();
-    let in_model = InModel { sender: prod };
+    let (prod, cons) = ring_buffer.split();
+
+    // Create input model and input stream using that model
+    let in_model = InputModel { producer: prod };
     let in_stream = audio_host
         .new_input_stream(in_model)
         .capture(pass_in)
         .build()
         .unwrap();
 
-    let out_model = OutModel { receiver: cons };
+    // Create output model and output stream using that model
+    let out_model = OutputModel { consumer: cons };
     let out_stream = audio_host
         .new_output_stream(out_model)
         .render(pass_out)
@@ -53,15 +60,15 @@ fn model(app: &App) -> Model {
     }
 }
 
-fn pass_in(model: &mut InModel, buffer: &Buffer) {
+fn pass_in(model: &mut InputModel, buffer: &Buffer) {
     for frame in buffer.frames() {
-        model.sender.push([frame[0], frame[1]]).unwrap();
+        model.producer.push([frame[0], frame[1]]).unwrap();
     }
 }
 
-fn pass_out(model: &mut OutModel, buffer: &mut Buffer) {
+fn pass_out(model: &mut OutputModel, buffer: &mut Buffer) {
     for frame in buffer.frames_mut() {
-        let recording_frame = match model.receiver.pop() {
+        let recording_frame = match model.consumer.pop() {
             Some(f) => f,
             None => [0.0, 0.0],
         };
@@ -74,13 +81,18 @@ fn pass_out(model: &mut OutModel, buffer: &mut Buffer) {
 fn key_pressed(_app: &App, model: &mut Model, key: Key) {
     match key {
         Key::Space => {
-            model.in_stream.play().unwrap();
-            model.out_stream.play().unwrap();
+            if model.in_stream.is_paused() {
+                model.in_stream.play().unwrap();
+                model.out_stream.play().unwrap();
+            } else {
+                model.in_stream.pause().unwrap();
+                model.out_stream.pause().unwrap();
+            }
         }
         _ => {}
     }
 }
 
-fn view(app: &App, model: &Model, frame: Frame) {
+fn view(_app: &App, _model: &Model, frame: Frame) {
     frame.clear(DIMGRAY);
 }
