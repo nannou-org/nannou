e9d81e515e1b77e353022827618ace6b38324363
Eirik Blekesaune <eirik.blekesaune@gmail.com>
2021-12-11 13:43:01 +0100

    Added cfg toggle for unix family targets

--- nannou/src/window.rs
+++ nannou/src/window.rs
@@ -755,9 +755,11 @@ impl<'app> Builder<'app> {
             }
         }
 
-        // Set the class type for X11
-        use winit::platform::unix::WindowBuilderExtUnix;
-        window = window.with_class("nannou".to_string(), "nannou".to_string());
+        // Set the class type for X11 if running unix family.
+        if cfg!(unix) {
+            use winit::platform::unix::WindowBuilderExtUnix;
+            window = window.with_class("nannou".to_string(), "nannou".to_string());
+        }
 
         // Set default dimensions in the case that none were given.
         let initial_window_size = window
