5a3caa045da67d8c4f8cd21bf98357e268731629
Jorik <vanveenjorik@gmail.com>
2022-06-02 13:58:18 +0200

    Add all UI except position and draw ellipse on the screen that uses the settings struct

--- examples/ui/egui/simple_ui.rs
+++ examples/ui/egui/simple_ui.rs
@@ -7,44 +7,90 @@ fn main() {
         .run();
 }
 
+struct Settings {
+    resolution: u32,
+    scale: f32,
+    rotation: f32,
+    color: Srgb<u8>,
+    position: Vec2,
+}
+
 struct Model {
+    settings: Settings, 
     egui: Egui
 }
 
 fn model(app: &App) -> Model {
+    // Create window
     let window_id = app.new_window()
         .view(view)
         .raw_event(raw_window_event)
         .build()
         .unwrap();
-
     let window = app.window(window_id).unwrap();
 
     let egui = Egui::from_window(&window);
     
     Model {
-        egui
+        egui,
+        settings: Settings {
+            resolution: 10,
+            scale: 200.0,
+            rotation: 0.0,
+            color: WHITE,
+            position: vec2(0.0, 0.0)
+        }
     }
 }
 
 fn update(_app: &App, model: &mut Model, update: Update) {
     let egui = &mut model.egui;
+    let settings = &mut model.settings;
 
     egui.set_elapsed_time(update.since_start);
     let ctx = egui.begin_frame();
 
-    egui::Window::new("Test").show(&ctx, |ui| {
-        ui.add(egui::Checkbox::new(&mut true, "test"));
+    egui::Window::new("Settings").show(&ctx, |ui| {
+        // Resolution slider
+        ui.add(egui::Label::new("Resolution:"));
+        ui.add(egui::Slider::new(&mut settings.resolution, 1..=40));
+
+        // Scale slider
+        ui.add(egui::Label::new("Scale:"));
+        ui.add(egui::Slider::new(&mut settings.scale, 0.0..=1000.0));
+
+        // Rotation slider
+        ui.add(egui::Label::new("Rotation:"));
+        ui.add(egui::Slider::new(&mut settings.rotation, 0.0..=360.0));
+
+        // Random color button
+        let clicked = ui.add(egui::Button::new("Random color"))
+            .clicked();
+
+        if clicked {
+            settings.color = rgb(random(), random(), random());
+        }
     });
 }
 
 fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    // Let egui handle things like keyboard and mouse input.
     model.egui.handle_raw_event(event);
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
+    let settings = &model.settings;    
+
     let draw = app.draw();
     draw.background().color(BLACK);
+
+    let rotation_radians = settings.rotation / 360.0 * 2.0 * std::f32::consts::PI;
+    draw.ellipse()
+        .resolution(settings.resolution as f32)
+        .xy(settings.position)
+        .color(settings.color)
+        .rotate(-rotation_radians)
+        .radius(settings.scale);
     
     draw.to_frame(app, &frame).unwrap();
     model.egui.draw_to_frame(&frame).unwrap();
