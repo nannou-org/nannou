d1339037d94c2762168d082857e41fa37d7893cd
mitchmindtree <mail@mitchellnordine.com>
2024-01-18 02:05:50 +1100

    Fix shell.nix on darwin

--- shell.nix
+++ shell.nix
@@ -1,10 +1,15 @@
-{ nannou
+{ lib
+, nannou
 , mkShell
+, stdenv
 }:
 mkShell {
   name = "nannou-dev";
   inputsFrom = [ nannou ];
-  env = {
-    inherit (nannou) ALSA_LIB_DEV LD_LIBRARY_PATH XCURSOR_THEME;
-  };
+  env = (lib.optionalAttrs stdenv.isLinux
+    {
+      inherit (nannou) ALSA_LIB_DEV LD_LIBRARY_PATH XCURSOR_THEME;
+    } // lib.optionalAttrs stdenv.isDarwin {
+    inherit (nannou) COREAUDIO_SDK_PATH;
+  });
 }
