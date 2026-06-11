970b09802f15018203be4c96f1f92ee22f44b273
GitHub Action <action@github.com>
2026-06-11 11:13:06 +0000

    Automated commit in preparation for deployment: 934

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-48fe8330.js',
+                path_to_root + 'searchindex-744b2a6b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
