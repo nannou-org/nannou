028974122096d84360a28d54344e5ae6b9bc0d83
GitHub Action <action@github.com>
2026-07-15 15:02:24 +0000

    Automated commit in preparation for deployment: 26

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5c3bab5e.js',
+                path_to_root + 'searchindex-cabf821b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
